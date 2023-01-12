b8b4a070d6079d6705563b553270c688b907e82f
sayyidmuhammada <sayyidmuhammad878@gmail.com>
2023-01-12 19:25:00 +0700

    fix some issues & make more improvments & add some other pages

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Header() {
+    const [scrollY, setScrollY] = useState(0);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        setScrollY(window.scrollY);
+      };
+  
+      handleScroll();
+  
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+  
+    }, []);
     return (
-        <header>
             <div className="header-area header-transparent">
                 <div className="main-header ">
-                    <div className="header-bottom  header-sticky">
+                    <div className={`header-bottom  header-sticky ${scrollY > 400 ? "sticky-bar": ""}`} style={{ transition: "all 0.5s ease-in" }}>
                         <div className="container-fluid">
                             <div className="row align-items-center">
                                 <div className="col-xl-2 col-lg-2">
                                     <div className="logo">
-                                        <a href="index.html"><img src="/assets/img/logo/logo.png" alt="" /></a>
+                                        <a href="/"><img src="/assets/img/logo/logo.png" alt="" /></a>
                                     </div>
                                 </div>
                                 <div className="col-xl-10 col-lg-10">
@@ -18,19 +32,10 @@ function Header() {
                                         <div className="main-menu d-none d-lg-block">
                                             <nav>
                                                 <ul id="navigation">
-                                                    <li className="active" ><a href="index.html">Home</a></li>
-                                                    <li><a href="courses.html">Courses</a></li>
-                                                    <li><a href="about.html">About</a></li>
-                                                    <li><a href="#">Blog</a>
-                                                        <ul className="submenu">
-                                                            <li><a href="blog.html">Blog</a></li>
-                                                            <li><a href="blog_details.html">Blog Details</a></li>
-                                                            <li><a href="elements.html">Element</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <li><a href="contact.html">Contact</a></li>
-                                                    <li className="button-header margin-left "><a href="#" className="btn">Join</a></li>
-                                                    <li className="button-header"><a href="login.html" className="btn btn3">Log in</a></li>
+                                                    <li className="active" ><a href="/">Home</a></li>
+                                                    <li><a href="courses">Courses</a></li>
+                                                    <li><a href="about">About</a></li>
+                                                    <li><a href="contact">Contact</a></li>
                                                 </ul>
                                             </nav>
                                         </div>
@@ -44,7 +49,6 @@ function Header() {
                     </div>
                 </div>
             </div>
-        </header>
     )
 }
 

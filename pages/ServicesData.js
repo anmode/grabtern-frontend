8574f5923cf8c6b125550ac38742c7ee81350440
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-01-13 12:28:51 +0530

    Changes UX of website

--- pages/ServicesData.js
+++ pages/ServicesData.js
@@ -2,19 +2,19 @@ export default [
     {
         imageSrc: "/assets/img/icon/icon1.svg",
         imageAlt: "",
-        serviceHeading: "60+ UX courses",
-        serviceDescription: "The automated process all your website tasks."
+        serviceHeading: "40+ Dream Internships",
+        serviceDescription: "Grabtern Contains the dream Internship and global hackathons."
     },
     {
         imageSrc: "/assets/img/icon/icon2.svg",
         imageAlt: "",
-        serviceHeading: "Expert instructors",
-        serviceDescription: "The automated process all your website tasks."
+        serviceHeading: "Former Interns",
+        serviceDescription: "Get the mentorship from the past fellows and increase your chance to get your internship"
     },
     {
         imageSrc: "/assets/img/icon/icon3.svg",
         imageAlt: "",
         serviceHeading: "Life time access",
-        serviceDescription: "The automated process all your website tasks."
+        serviceDescription: "Once you purchased the targetted intern your mentorship program begins"
     }
 ]

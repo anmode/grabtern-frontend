d33bfb03ad90f9a21fde59563f03a642d7cbf8a8
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-01-16 13:07:15 +0530

    Early bird

--- pages/allCoursesData.js
+++ pages/allCoursesData.js
@@ -7,7 +7,7 @@ export default [
         courseDescription: "It's a global program focused on bringing more student developers into open source software development. Google Summer of Code is a global program that offers student developers stipends to write code for various open source software projects.",
         courseRating: 4.9,
         coursePayed: 450,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/mlhprep.png",
@@ -17,7 +17,7 @@ export default [
         courseDescription: "MLH Prep is a free, self-paced, online learning program that helps students prepare for MLH Fellowship, an open-source development program for students interested in software development. The program is designed to help students improve their skills and increase their chances of being accepted into the MLH Fellowship.",
         courseRating: 4.5,
         coursePayed: 250,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/mlhfellowship.png",
@@ -27,7 +27,7 @@ export default [
         courseDescription: "The MLH Fellowship is open to students of all majors and skill levels, as long as they are 18 years or older and are currently enrolled in a degree-granting program or have graduated within the last 12 months.",
         courseRating: 4.9,
         coursePayed: 1200,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/adobeshe.png",
@@ -37,7 +37,7 @@ export default [
         courseDescription: "Adobe SHE as a resource for female and non-binary students interested in technology and provide guidance on how to apply and what to expect if accepted. The program can provide great opportunities for networking, mentorship, and scholarships.",
         courseRating: 4.5,
         coursePayed: 120,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/mitacs.png",
@@ -47,7 +47,7 @@ export default [
         courseDescription: "Mitacs GRI as an opportunity for students who are interested in conducting research abroad and provide guidance on how to apply and what to expect if accepted.It is worth noting that the program might have specific requirements or eligibility criteria, and it's worth checking the official website or contacting Mitacs to know more about the program, its availability, the application process and the benefits that the students could get from it.",
         courseRating: 4.5,
         coursePayed: 120,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/grid.png",
@@ -57,7 +57,7 @@ export default [
         courseDescription: "Flipkart, an Indian e-commerce company, to provide research opportunities for graduate students in various areas such as Machine Learning, Computer Vision, Natural Language Processing, Recommender Systems and more. The program aims to provide students with an opportunity to conduct cutting-edge research in a collaborative and fast-paced environment, and to work on real-world industry problems.",
         courseRating: 4.8,
         coursePayed: 670,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/step.png",
@@ -67,7 +67,7 @@ export default [
         courseDescription: "It is a 12-week internship program designed for undergraduate students who are in their first or second year of studies and are interested in pursuing a career in technology. The program is designed to provide students with hands-on experience in software engineering and technology, as well as opportunities for professional development and networking. During the internship, students will work on real-world projects and gain valuable industry experience in areas such as software engineering, user experience, and product management.",
         courseRating: 4.8,
         coursePayed: 670,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/wow.png",
@@ -77,7 +77,7 @@ export default [
         courseDescription: "Amazon Web Services (AWS) Worldwide Operations (WWOps) Internship Program, also known as 'AWS WOW Internship Program' is a 12-week internship program that provides students with the opportunity to work with Amazon Web Services (AWS) Worldwide Operations (WWOps) team and gain hands-on experience in areas such as Data Center Operations, Network Engineering, IT Support, and more. The program is designed to give students an opportunity to work on real-world projects and gain valuable industry experience.",
         courseRating: 4.8,
         coursePayed: 670,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/hackerrank.png",
@@ -87,7 +87,7 @@ export default [
         courseDescription: "HackerRank internships are online, remote internships where students work on real-world projects and contribute to the company's codebase, just like a regular on-site internship. It's a great way for students to gain practical experience and to improve their coding skills while they are pursuing their education",
         courseRating: 4.8,
         coursePayed: 800,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/isro.png",
@@ -97,7 +97,7 @@ export default [
         courseDescription: "ISRO provides internships for students who are interested in space technology and related fields. These internships are typically offered to undergraduate and graduate students, as well as postdoctoral researchers. The duration of the internship can vary depending on the program, but it can range from a few weeks to several months.",
         courseRating: 4.7,
         coursePayed: 270,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/mlamazon.png",
@@ -107,7 +107,7 @@ export default [
         courseDescription: "The Amazon ML Summer Internship program provides students with the opportunity to work on real-world projects with Amazon's Machine Learning experts. Interns will have the opportunity to gain hands-on experience with the latest machine learning technologies and tools, work on cutting-edge research and development projects, and collaborate with other interns and mentors from different teams.",
         courseRating: 4.8,
         coursePayed: 170,
-        coursePrice: 135
+        coursePrice: 1
     },
     {
         courseImage: "/assets/img/gallery/kde.png",
@@ -117,7 +117,7 @@ export default [
         courseDescription: "During the Season of Docs, technical writers work with open-source software development communities to improve documentation, create tutorials, and write user guides. Technical writers are paired with mentor developers from the participating open-source projects, who guide them through the process of contributing to the project's documentation.",
         courseRating: 4.8,
         coursePayed: 170,
-        coursePrice: 135
+        coursePrice: 1
     }
     
 ]

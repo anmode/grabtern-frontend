4b0225a922d2958e8966d67c03504f990e4544ee
sayyidmuhammada <khjifry@outlook.com>
2023-01-29 20:57:57 +0700

    change testimonials desc

--- frontend/pages/data/testiomialsData.js
+++ frontend/pages/data/testiomialsData.js
@@ -4,27 +4,27 @@ export default [
         testimonialUserHeadline: "Youtuber | Software Engineer",
         testimonialUserImage: "testimonial-profile1.png",
         testimonialRate: 4,
-        testimonialDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+        testimonialDescription: "GrabTern is very best plaftorm to Guide Internships by mentor they easly teach me step by step and I got one intern from GSoc. I very preffered for you who are graduated from University and not yet get job / intern, because they help me a lot, Thanks to Grabtern"
     },
     {
         testimonialUserName: "Any",
         testimonialUserHeadline: "Youtuber | Blogger",
         testimonialUserImage: "testimonial-profile1.png",
         testimonialRate: 4.5,
-        testimonialDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+        testimonialDescription: "I have been using GrabTern for a few days ago a I love it! I can't imagine he guide me about Amazon ML Summer Internship easly and also they have customer and contact 24/7 it is so great. Thank you"
     },
     {
         testimonialUserName: "Emma",
         testimonialUserHeadline: "Technical Content Writer",
         testimonialUserImage: "testimonial-profile1.png",
         testimonialRate: 3.5,
-        testimonialDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+        testimonialDescription: "Would definitely recommend GrabTern and will definitely sharing to all my friends to be ordering again"
     },
     {
         testimonialUserName: "Ava",
         testimonialUserHeadline: "Youtuber | Web Developer",
         testimonialUserImage: "testimonial-profile1.png",
         testimonialRate: 4,
-        testimonialDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+        testimonialDescription: "If you want to get a new intern that paid around $3000, use GrabTern and don't look any further. Thank you to GrabTern"
     },
 ]

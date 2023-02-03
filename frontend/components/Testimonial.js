ccd54a436de594e365a9623e453c6b21cb7414c1
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-02-04 00:40:23 +0530

    testemonial data and grabtern community section updated

--- frontend/components/Testimonial.js
+++ frontend/components/Testimonial.js
@@ -1,29 +1,45 @@
-import React from 'react'
+import React from "react";
 
-function Testimonial({ testimonialUserName, testimonialUserHeadline, testimonialUserImage, testimonialRate, testimonialDescription }) {
+function Testimonial({
+  testimonialUserName,
+  testimonialUserHeadline,
+  testimonialUserImage,
+  testimonialRate,
+  testimonialDescription,
+}) {
   return (
-    <div className='box'>
-      <i className='fas fa-quote-left quote'></i>
-      <p>{testimonialDescription}</p>
-      <div className='content'>
-        <div className='info'>
-          <div className='name'>{testimonialUserName}</div>
-          <div className='job'>{testimonialUserHeadline}</div>
-          <div className='stars'>
-            {Array(Math.floor(testimonialRate)).fill("").map((_, index) => (
-              <i key={index} className="fas fa-star"></i>
-            ))}
+    <div className="box">
+      <i className="fas fa-quote-left quote"></i>
+      <p>
+        {testimonialDescription.length < 400
+          ? testimonialDescription
+          : testimonialDescription.split(" ").slice(0, 400).join(" ") + "..."}
+      </p>
+      <div className="content">
+        <div className="info">
+          <div className="name">{testimonialUserName}</div>
+          <div className="job">{testimonialUserHeadline}</div>
+          <div className="stars">
+            {Array(Math.floor(testimonialRate))
+              .fill("")
+              .map((_, index) => (
+                <i key={index} className="fas fa-star"></i>
+              ))}
 
-            {testimonialRate.toString().includes('.') ? (<i className="fas fa-star-half"></i>) : null}
+            {testimonialRate.toString().includes(".") ? (
+              <i className="fas fa-star-half"></i>
+            ) : null}
           </div>
-
         </div>
-        <div className='image'>
-          <img src={`/assets/img/testimonials/${testimonialUserImage}`} alt="testimonial-profile" />
+        <div className="image">
+          <img
+            src={`/assets/img/testimonials/${testimonialUserImage}`}
+            alt="testimonial-profile"
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Testimonial
+export default Testimonial;
--- frontend/pages/index.page.js
+++ frontend/pages/index.page.js
@@ -280,7 +280,7 @@ export default function Home() {
             <div className="row justify-content-center">
               <div className="col-xl-7 col-lg-8">
                 <div className="section-tittle text-center mb-55">
-                  <h2>Community Mentors</h2>
+                  <h2>Grabtern Community</h2>
                 </div>
               </div>
             </div>

9f755982a7ecef19e6b992cd762526c7b43a91c9
sayyidmuhammada <sayyidmuhammad878@gmail.com>
2023-02-06 15:55:16 +0700

    done all tasks and fix all issues

---
+++ backend/routes/mentor/mentorVerify.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { Mentor } = require("../../models/mentor");
+const crypto = require("crypto")
+
+router.get('/:token', async (req, res) => {
+    try {
+        let mentorVerifyToken = req.params.token;
+        const mentor = await Mentor.find({ token: mentorVerifyToken });
+        console.log(mentor[0])
+        if (mentor.length === 0) return res.status(400).send({ message: "Invalid link" });
+        if (mentor[0].verified === true) return res.status(200).send({ message: "This mentor is already verified!" })
+        const mentorSetupPWCode = crypto.randomBytes(16).toString('hex');
+        await Mentor.updateOne({ token: mentorVerifyToken }, { setupPWId: mentorSetupPWCode })
+        await Mentor.updateOne({ token: mentorVerifyToken }, { verified: true });
+        await Mentor.updateOne({ token: mentorVerifyToken }, { token: "mentorIsVerified" });
+        console.log(mentor[0]);
+        return res.send(`Mentor with email ${mentor[0].email} successfully verified! and please send this message to mentor to setup there password \nSetup for your password click this link: http://localhost:3000/mentorPWSetup/${mentorSetupPWCode}`)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+module.exports = router

a7efc2df52e5bdcb168fcd77d0adc128c38ca0d5
sayyidmuhammada <sayyidmuhammad878@gmail.com>
2023-02-14 05:33:52 +0700

    done all tasks and issue fix

--- backend/routes/mentor/getMentorDetail.js
+++ backend/routes/mentor/getMentorDetail.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const { Mentor } = require("../../models/mentor");
 
-router.get('/:mentorName', async (req, res) => {
+router.get('/:mentorUsername', async (req, res) => {
     try {
-        let mentorName = req.params.mentorName;
-        const mentor = await Mentor.find({ name: mentorName });
+        let mentorUsername = req.params.mentorUsername;
+        const mentor = await Mentor.find({ username: mentorUsername });
         if(mentor.length === 0) return res.status(200).send({message: "Invalid link"});
         return res.status(200).send({mentorDetail: mentor[0]})
     } catch (err) {

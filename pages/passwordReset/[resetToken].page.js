21c12af49d2d9e8fabdd131bd1ebf86d32b8cbe6
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-04-19 10:44:09 +0530

    fixed build

--- pages/passwordReset/[resetToken].page.js
+++ pages/passwordReset/[resetToken].page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 const Header = dynamic(() => import("../../components/Header"));
-import ResetPassword from "../components/ResetPassword";
+import ResetPassword from "../../components/resetpassword";
 
 const Footer = dynamic(() => import("../../components/Footer"));
 

3f43328f8f0b3058b5b2252c848fa1fb04f78864
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-04-19 10:42:16 +0530

    yeh formatting sucks

--- pages/passwordReset/[resetToken].page.js
+++ pages/passwordReset/[resetToken].page.js
@@ -1,20 +1,20 @@
 import React from "react";
 import dynamic from "next/dynamic";
 const Header = dynamic(() => import("../../components/Header"));
-import ResetPassword from '../components/ResetPassword';
+import ResetPassword from "../components/ResetPassword";
 
 const Footer = dynamic(() => import("../../components/Footer"));
 
-function Index({resetToken }) {
-    return (
-      <>
-        <Header navbarBackground={true} />
-        <main>
+function Index({ resetToken }) {
+  return (
+    <>
+      <Header navbarBackground={true} />
+      <main>
         <ResetPassword resetToken={resetToken} />
-        </main>
-        <Footer />
-      </>
-    );
-  }
-  
-  export default Index;
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Index;

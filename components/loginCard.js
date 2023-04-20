1a016fc322f4d29c081058743e2462036f266b55
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-04-20 13:25:13 +0530

    lint fixed

--- components/loginCard.js
+++ components/loginCard.js
@@ -1,22 +1,19 @@
 import React from "react";
 
-
 function LoginCard(props) {
-
-  
   return (
     <div className="card" onMouseLeave={props.hideCard}>
       <div className="content">
         <div className="buttons">
           <button
             className="user-button"
-            onClick={() => window.location.href = "/login"}
+            onClick={() => (window.location.href = "/login")}
           >
             Login as User
           </button>
           <button
             className="mentor-button"
-            onClick={() => window.location.href = "/mentorLogin"}
+            onClick={() => (window.location.href = "/mentorLogin")}
           >
             Login as Mentor
           </button>

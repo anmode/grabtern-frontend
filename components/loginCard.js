2de8baab5ff5ea344a713d0515770b7b7f5b6421
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-04-20 12:53:01 +0530

    modified the css and functionality

--- components/loginCard.js
+++ components/loginCard.js
@@ -1,20 +1,22 @@
 import React from "react";
 
+
 function LoginCard(props) {
+
+  
   return (
     <div className="card" onMouseLeave={props.hideCard}>
-      <div className="background" />
       <div className="content">
         <div className="buttons">
           <button
             className="user-button"
-            onClick={() => console.log("User Login")}
+            onClick={() => window.location.href = "/login"}
           >
             Login as User
           </button>
           <button
             className="mentor-button"
-            onClick={() => console.log("Mentor Login")}
+            onClick={() => window.location.href = "/mentorLogin"}
           >
             Login as Mentor
           </button>

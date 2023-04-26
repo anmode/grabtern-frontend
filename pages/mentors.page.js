d0d5e244a0460f80bc4def390ac77a8b8db0f129
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-04-26 08:05:46 +0530

    removed unused part

--- pages/mentors.page.js
+++ pages/mentors.page.js
@@ -21,32 +21,6 @@ function Mentors({ mentorsData }) {
                 {mentorsData.map((mentor) => (
                   <a href={`/${mentor.username}`} key={mentor._id}>
                     {
-                      /* <div className="mentorCard">
-                      <img src={mentor.mentorImg} alt="exampleMentorPhoto" />
-                      <h2 className="mentorName">{mentor.name}</h2>
-                      <div
-                        className="contactLinks"
-                        style={{ marginBottom: "10px" }}
-                      >
-                        <a href={`/mentors/${mentor.username}`} target="_blank">
-                          <i class="fas fa-envelope"></i>
-                        </a>
-                        <a href={`${mentor.social.linkedin}`} target="_blank">
-                          <i class="fab fa-linkedin"></i>
-                        </a>
-                        <a href={`${mentor.social.twitter}`} target="_blank">
-                          <i class="fab fa-twitter"></i>
-                        </a>
-                      </div>
-                      <h3>Intern at: {mentor.internAt}</h3>
-                      <h3>{mentor.currentStatus}</h3>
-                      <p>
-                        {mentor.description?.length > 120
-                          ? `${mentor.description.substring(0, 120)}…`
-                          : mentor.description}
-                      </p>
-                      { <h3>Price for each intern: {mentor.sessionPrice}</h3> }
-                    </div> */
                       <MentorCard mentor={mentor} />
                     }
                   </a>

cf7945b6a9f48fd4011ffb1db54ba3993cd215f5
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-04-26 08:45:45 +0530

    fix and using render

--- pages/mentors.page.js
+++ pages/mentors.page.js
@@ -1,32 +1,36 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import dynamic from "next/dynamic";
 import MentorCard from "../components/mentor";
-const Header = dynamic(() => import("../components/Header"));
-const SimpleBanner = dynamic(() => import("../components/SimpleBanner"));
-import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+import Header from "../components/Header";
+import SimpleBanner from "../components/SimpleBanner";
 import { Spin } from "antd";
 
-const queryClient = new QueryClient();
-
-function Mentors() {
+const Mentors = () => {
+  const [mentorsData, setMentorsData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-  const { data: mentorsData, isError, error } = useQuery(
-    "mentors",
-    async () => {
-      const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists`;
-      const { data } = await axios.get(url);
-      return data.filter(
-        (mentor) =>
-          mentor.verified === true && mentor.token === "mentorIsVerified"
-      );
-    },
-    {
-      refetchOnWindowFocus: false,
-      onSuccess: () => setIsLoading(false),
-    }
-  );
+  useEffect(() => {
+    const fetchMentors = async () => {
+      try {
+        const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists`;
+        const { data } = await axios.get(url);
+        setMentorsData(
+          data.filter(
+            (mentor) =>
+              mentor.verified === true && mentor.token === "mentorIsVerified"
+          )
+        );
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMentors();
+  }, []);
 
   return (
     <>
@@ -35,20 +39,20 @@ function Mentors() {
       <main>
         <section className="findMentors">
           <div className="container">
-            <h1>Find All mentors here's</h1>
-            {isError ? (
-              <p>{error.message}</p>
-            ) : isLoading ? (
+            <h1>Find all mentors here</h1>
+            {isLoading ? (
               <div className="loadingSpinner">
                 <Spin size="large" />
               </div>
+            ) : isError ? (
+              <p>There was an error while fetching mentors.</p>
             ) : mentorsData.length === 0 ? (
-              <p>There is no mentor right now...</p>
+              <p>There are no mentors available right now.</p>
             ) : (
               <div className="mentorLists">
                 {mentorsData.map((mentor) => (
                   <a href={`/${mentor.username}`} key={mentor._id}>
-                    {<MentorCard mentor={mentor} />}
+                    <MentorCard mentor={mentor} />
                   </a>
                 ))}
               </div>
@@ -58,12 +62,6 @@ function Mentors() {
       </main>
     </>
   );
-}
+};
 
-export default function MentorsPage() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Mentors />
-    </QueryClientProvider>
-  );
-}
+export default Mentors;

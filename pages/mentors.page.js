c7bfa30c1e1731af86842dde269c838f82f40c22
glokta1 <rafeyahmad@protonmail.com>
2023-04-29 12:05:26 +0530

    Revert mentors page to stable state

--- pages/mentors.page.js
+++ pages/mentors.page.js
@@ -1,37 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 import dynamic from "next/dynamic";
 import MentorCard from "../components/mentor";
 const Header = dynamic(() => import("../components/Header"));
 const SimpleBanner = dynamic(() => import("../components/SimpleBanner"));
-import { useQuery, QueryClient, QueryClientProvider } from "react-query";
-import { Spin } from "antd";
 
-const queryClient = new QueryClient();
-
-function Mentors() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const {
-    data: mentorsData,
-    isError,
-    error,
-  } = useQuery(
-    "mentors",
-    async () => {
-      const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists`;
-      const { data } = await axios.get(url);
-      return data.filter(
-        (mentor) =>
-          mentor.verified === true && mentor.token === "mentorIsVerified"
-      );
-    },
-    {
-      refetchOnWindowFocus: false,
-      onSuccess: () => setIsLoading(false),
-    }
-  );
-
+function Mentors({ mentorsData }) {
   return (
     <>
       <Header />
@@ -40,19 +14,41 @@ function Mentors() {
         <section className="findMentors">
           <div className="container">
             <h1>Find All mentors here's</h1>
-            {isError ? (
-              <p>{error.message}</p>
-            ) : isLoading ? (
-              <div className="loadingSpinner">
-                <Spin size="large" />
-              </div>
-            ) : mentorsData.length === 0 ? (
+            {mentorsData.length === 0 ? (
               <p>There is no mentor right now...</p>
             ) : (
               <div className="mentorLists">
                 {mentorsData.map((mentor) => (
                   <a href={`/${mentor.username}`} key={mentor._id}>
-                    {<MentorCard mentor={mentor} />}
+                    {
+                      /* <div className="mentorCard">
+                      <img src={mentor.mentorImg} alt="exampleMentorPhoto" />
+                      <h2 className="mentorName">{mentor.name}</h2>
+                      <div
+                        className="contactLinks"
+                        style={{ marginBottom: "10px" }}
+                      >
+                        <a href={`/mentors/${mentor.username}`} target="_blank">
+                          <i class="fas fa-envelope"></i>
+                        </a>
+                        <a href={`${mentor.social.linkedin}`} target="_blank">
+                          <i class="fab fa-linkedin"></i>
+                        </a>
+                        <a href={`${mentor.social.twitter}`} target="_blank">
+                          <i class="fab fa-twitter"></i>
+                        </a>
+                      </div>
+                      <h3>Intern at: {mentor.internAt}</h3>
+                      <h3>{mentor.currentStatus}</h3>
+                      <p>
+                        {mentor.description?.length > 120
+                          ? `${mentor.description.substring(0, 120)}…`
+                          : mentor.description}
+                      </p>
+                      { <h3>Price for each intern: {mentor.sessionPrice}</h3> }
+                    </div> */
+                      <MentorCard mentor={mentor} />
+                    }
                   </a>
                 ))}
               </div>
@@ -64,10 +60,18 @@ function Mentors() {
   );
 }
 
-export default function MentorsPage() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Mentors />
-    </QueryClientProvider>
-  );
-}
+export default Mentors;
+
+export const getServerSideProps = async (context) => {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists`;
+  const { data } = await axios.get(url);
+
+  return {
+    props: {
+      mentorsData: data.filter(
+        (mentor) =>
+          mentor.verified === true && mentor.token === "mentorIsVerified"
+      ),
+    },
+  };
+};

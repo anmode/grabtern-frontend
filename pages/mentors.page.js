9375d797f9c74c22be1cd2d770dbe4b4f53303b2
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-05-02 16:54:02 +0530

    fixed the mentor page

--- pages/mentors.page.js
+++ pages/mentors.page.js
@@ -1,37 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 import dynamic from "next/dynamic";
 import MentorCard from "../components/mentor";
 const Header = dynamic(() => import("../components/Header"));
 const SimpleBanner = dynamic(() => import("../components/SimpleBanner"));
-import { useQuery, QueryClient, QueryClientProvider } from "react-query";
-import { Spin } from "antd";
 
-const queryClient = new QueryClient();
-
-function Mentors() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const {
-    data: mentorsData,
-    isError,
-    error,
-  } = useQuery(
-    "mentors",
-    async () => {
-      const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists`;
-      const { data } = await axios.get(url);
-      return data.filter(
-        (mentor) =>
-          mentor.verified === true && mentor.token === "mentorIsVerified"
-      );
-    },
-    {
-      refetchOnWindowFocus: false,
-      onSuccess: () => setIsLoading(false),
-    }
-  );
-
+function Mentors({ mentorsData }) {
   return (
     <>
       <Header />
@@ -40,13 +14,7 @@ function Mentors() {
         <section className="findMentors">
           <div className="container">
             <h1>Find All mentors here's</h1>
-            {isError ? (
-              <p>{error.message}</p>
-            ) : isLoading ? (
-              <div className="loadingSpinner">
-                <Spin size="large" />
-              </div>
-            ) : mentorsData.length === 0 ? (
+            {mentorsData.length === 0 ? (
               <p>There is no mentor right now...</p>
             ) : (
               <div className="mentorLists">
@@ -64,10 +32,19 @@ function Mentors() {
   );
 }
 
-export default function MentorsPage() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Mentors />
-    </QueryClientProvider>
-  );
-}
+export default Mentors;
+
+export const getStaticProps = async (context) => {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists`;
+  const { data } = await axios.get(url);
+
+  return {
+    props: {
+      mentorsData: data.filter(
+        (mentor) =>
+          mentor.verified === true && mentor.token === "mentorIsVerified"
+      ),
+    },
+    revalidate: 20, // revalidate the data every 20 seconds
+  };
+};

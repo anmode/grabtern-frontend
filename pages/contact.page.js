34b86dfd947e3b5fcb4c7530f466ffec0bc1ceb2
pragatijain <jpragati3003@gmail.com>
2023-06-03 11:45:59 +0530

    contact page css module updated

--- pages/contact.page.js
+++ pages/contact.page.js
@@ -1,7 +1,8 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import dynamic from "next/dynamic";
-// import style from "../styles/contact.css"
+import style from "../styles/contact.module.css";
+
 const Footer = dynamic(() => import("../components/Footer"));
 const Header = dynamic(() => import("../components/Header"));
 const SimpleBanner = dynamic(() => import("../components/SimpleBanner"));
@@ -39,26 +40,66 @@ function Contact() {
     <>
       <Header />
       <SimpleBanner bannerTittle="Contact us" siteName="Contact" />
-      <main>
-        <section className="contact-section">
+      <main className={style.main}>
+        <div className={style.mainbody} >
+        <section className={style.csection}>
           <div className="container">
             <div className="d-none d-sm-block mb-5 pb-4"></div>
-            <div className="row ">
+            <div className="row">
               <div className="col-12">
           
               </div>
-              <div className="col-lg-8">
+              <div className={`col-lg-8 ${style.flexx}`}>
+              <div className={`${style.offsetlg} col-lg-3`}>
+                <div className={`${style.contactinfo} media`}>
+                 
+                  <div className={style.media_head}>
+                    <h3>Contact Information</h3>
+                  </div>
+                </div>
+                  <div class={style.centerr}>
+                <div className={`${style.contactinfo} media`}>
+                  <span className={style.contactinfo__icon}>
+                    <i className="ti-home" />
+                  </span>
+                  <div className={style.media_body}>
+                    <h3>Kishanpur</h3>
+                    <p>Aligarh, Uttar Pradesh</p>
+                  </div>
+                </div>
+                <div className={`${style.contactinfo} media`}>
+                  <span className={style.contactinfo__icon}>
+                    <i className="ti-tablet" />
+                  </span>
+                  <div className={style.media_body}>
+                    <h3>9368086395</h3>
+                    <p>Mon to Fri 9am to 6pm</p>
+                  </div>
+                </div>
+                <div className={`${style.contactinfo} media`}>
+                  <span className={style.contactinfo__icon}>
+                     <i className="ti-email" />
+                  </span>
+                  <div className={style.media_body}>
+                    <h3>contact.grabtern@gmail.com</h3>
+                    <p>Send us your query anytime!</p>
+                  </div>
+                </div>
+                </div>
+              </div>
                 <form
                   className="form-contact contact_form"
                   id="contactForm"
                   ref={form}
                   onSubmit={sendEmail}
                 >
-                    <h2 className="contact-title">Get in Touch</h2>
-                  <div className="row contactf">
-                
+                    
+                  <div className={`${style.cf} row`}>
+                    
+                  <h2 className={style.ctitle}>Get in Touch</h2>
+
                     <div className="col-12">
-                      <div className="form-group">
+                      <div className={style.fgroup}>
                         <textarea
                           className="form-control w-100"
                           name="message"
@@ -74,7 +115,7 @@ function Contact() {
                       </div>
                     </div>
                     <div className="col-sm-6">
-                      <div className="form-group">
+                      <div className={style.fgroup}>
                         <input
                           className="form-control valid"
                           name="name"
@@ -89,7 +130,7 @@ function Contact() {
                       </div>
                     </div>
                     <div className="col-sm-6">
-                      <div className="form-group">
+                      <div className={style.fgroup}>
                         <input
                           className="form-control valid"
                           name="email"
@@ -104,7 +145,7 @@ function Contact() {
                       </div>
                     </div>
                     <div className="col-12">
-                      <div className="form-group">
+                      <div className={style.fgroup}>
                         <input
                           className="form-control"
                           name="subject"
@@ -118,10 +159,10 @@ function Contact() {
                         />
                       </div>
                     </div>
-                    <div className="form-group mt-3">
+                    <div className={`${style.fgroup} mt-3`}>
                     <button
                       type="submit"
-                      className="button button-contactForm boxed-btn"
+                      className={style.boxedbtn}
                     >
                       Send
                     </button>
@@ -129,39 +170,49 @@ function Contact() {
                   </div>
                  
                 </form>
-              </div>
-              <div className="col-lg-3 offset-lg-1">
-                <div className="media contact-info">
-                  <span className="contact-info__icon">
+                {/* <div className={`${style.offsetlg} col-lg-3`}>
+                <div className={`${style.contactinfo} media`}>
+                 
+                  <div className={style.media_head}>
+                    <h3>Contact Information</h3>
+                  </div>
+                </div>
+                  <div class={style.centerr}>
+                <div className={`${style.contactinfo} media`}>
+                  <span className={style.contactinfo__icon}>
                     <i className="ti-home" />
                   </span>
-                  <div className="media-body">
+                  <div className={style.media_body}>
                     <h3>Kishanpur</h3>
                     <p>Aligarh, Uttar Pradesh</p>
                   </div>
                 </div>
-                <div className="media contact-info">
-                  <span className="contact-info__icon">
+                <div className={`${style.contactinfo} media`}>
+                  <span className={style.contactinfo__icon}>
                     <i className="ti-tablet" />
                   </span>
-                  <div className="media-body">
+                  <div className={style.media_body}>
                     <h3>9368086395</h3>
                     <p>Mon to Fri 9am to 6pm</p>
                   </div>
                 </div>
-                <div className="media contact-info">
-                  <span className="contact-info__icon">
-                    <i className="ti-email" />
+                <div className={`${style.contactinfo} media`}>
+                  <span className={style.contactinfo__icon}>
+                     <i className="ti-email" />
                   </span>
-                  <div className="media-body">
+                  <div className={style.media_body}>
                     <h3>contact.grabtern@gmail.com</h3>
                     <p>Send us your query anytime!</p>
                   </div>
                 </div>
+                </div>
+              </div> */}
               </div>
+           
             </div>
           </div>
         </section>
+        </div>
       </main>
       <Footer />
     </>

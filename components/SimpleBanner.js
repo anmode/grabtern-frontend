238599ad559493ad47ef14d14c4a8fbe0215e375
Riyazul555 <riyazulislam2003@gmail.com>
2023-06-06 23:39:43 +0530

    added search bar and filter functionality

--- components/SimpleBanner.js
+++ components/SimpleBanner.js
@@ -1,6 +1,18 @@
 import React from "react";
+import styles from '../styles/filter.module.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function SimpleBanner({ bannerTittle, siteName }) {
+   const [nameList , setnameList] = useState([])
+   const [search , setsearch ] = useState ("")
+
+
+   useEffect(() =>{
+     axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists`)
+     .then((response) => {setnameList(response.data)})
+   },[])
+
   return (
     <section className="slider-area slider-area2">
       <div className="slider-active">
@@ -9,6 +21,21 @@ function SimpleBanner({ bannerTittle, siteName }) {
             <div className="row">
               <div className="col-xl-8 col-lg-11 col-md-12">
                 <div className="hero__caption hero__caption2">
+                  <div className={styles.global_search}>
+                    <input className={styles.search_bar} _ngcontent-d2c-frontend-c75="" type="text" autoComplete="off" placeholder="Search Mentors" onChange={(e) => setsearch(e.target.value
+                      )} />
+                      {nameList.filter((item) => {
+                        if(search===""){
+                          return item
+                        }
+                        else if(item.name.toLowerCase().includes(search.toLowerCase()) || (item.currentStatus.toLowerCase().includes(search.toLowerCase())) || (item.internAt.toLowerCase().includes(search.toLowerCase())))  {
+                          return item
+                        }
+                      })
+                      .map((item)=>{
+                        return <h4>{item.name}</h4>
+                      })}
+                  </div>
                   <h1 data-animation="bounceIn" data-delay="0.2s">
                     {bannerTittle}
                   </h1>

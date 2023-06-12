69d9ad44f64e52ebbab23658d828813f0f5828be
Vidip Ghosh <vidip2001@gmail.com>
2023-06-12 17:22:26 +0530

    Added dividing line between categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    divideWidth:{
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    }
   },
   plugins: [],
 }

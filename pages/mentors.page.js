502ec982842071075908f01350a7495665fcc42a
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-06-14 11:44:31 +0530

    added infinite scroll in mentorsList page

--- pages/mentors.page.js
+++ pages/mentors.page.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import dynamic from "next/dynamic";
 import MentorCard from "../components/mentor";
@@ -8,6 +8,52 @@ const SimpleBanner = dynamic(() => import("../components/SimpleBanner"));
 import Head from "next/head";
 
 function Mentors({ mentorsData }) {
+  const [data, setData] = useState(mentorsData);
+  const [limit, setLimit] = useState(6);
+  const [loading, setLoading] = useState(false);
+  const [skipvalue, setSkipValue] = useState(6);
+
+  const getMentorData = async () => {
+    setLoading(true);
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists?_limit=${limit}&_skipvalue=${skipvalue}`
+    );
+    const newData = await res.json();
+    const filterData = newData.filter(
+      (mentor) =>
+        mentor.verified === true && mentor.token === "mentorIsVerified"
+    );
+    setData([...data, ...filterData]);
+    setSkipValue(skipvalue + limit);
+    setLoading(false);
+    console.log("data here", newData);
+    // Add logic to update your state or perform other operations with the fetched data
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleInfiniteScroll);
+    return () => window.removeEventListener("scroll", handleInfiniteScroll);
+  }, []);
+
+  useEffect(() => {
+    getMentorData();
+    console.log(data);
+  }, [limit]);
+
+  const handleInfiniteScroll = async () => {
+    try {
+      if (
+        (window.innerHeight + document.documentElement.scrollTop )*1.5 >=
+        document.documentElement.scrollHeight
+      ) {
+        setLimit((prev) => prev + 6);
+        // setSkipValue(skipvalue+limit);
+      }
+    } catch (error) {
+      console.log("Error in handleInfiniteScroll function", error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -27,16 +73,23 @@ function Mentors({ mentorsData }) {
         <section className="findMentors">
           <div className="container">
             <h1>Find All Mentors Here</h1>
-            {mentorsData.length === 0 ? (
+            {data.length === 0 ? (
               <p>There is no mentor right now...</p>
             ) : (
               <div className="mentorLists">
-                {mentorsData.map((mentor) => (
+                {data.map((mentor) => (
                   <a href={`/${mentor.username}`} key={mentor._id}>
                     {<MentorCard mentor={mentor} />}
                   </a>
                 ))}
               </div>
+            )}
+            {loading && (
+              <img
+                style={{ width: "100px", height: "100px" }}
+                src="/assets/img/gif/Spinner.gif"
+                alt="...jljk"
+              />
             )}
           </div>
         </section>
@@ -48,10 +101,9 @@ function Mentors({ mentorsData }) {
 
 export default Mentors;
 
-export const getStaticProps = async (context) => {
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists`;
+export const getServerSideProps = async (context) => {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mentors/mentorLists?_limit=6&_skipvalue=0`;
   const { data } = await axios.get(url);
-
   return {
     props: {
       mentorsData: data.filter(
@@ -59,6 +111,5 @@ export const getStaticProps = async (context) => {
           mentor.verified === true && mentor.token === "mentorIsVerified"
       ),
     },
-    revalidate: 20, // revalidate the data every 20 seconds
   };
 };

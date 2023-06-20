4f039e1c5f2b5e367beacf6f6f4392aa535b6e8e
Archie Jain <jain.28.archie@gmail.com>
2023-06-20 21:50:12 +0530

    Added Spinner Component in the components/basic folder

---
+++ components/basic/spinner.js
@@ -0,0 +1,13 @@
+import {React} from 'react';
+
+
+const Spinner =()=>{
+    return (
+    <div class="h-screen bg-white">
+    <div class="flex justify-center items-center h-full">
+      <img class="h-16 w-16" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="" />
+    </div>
+    </div>)
+}
+
+export default Spinner;

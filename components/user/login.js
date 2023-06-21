6b712c6cfbe58404b7d4ba3916687e03a17105ca
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-06-22 00:48:19 +0530

    Added show password button

--- components/user/login.js
+++ components/user/login.js
@@ -1,15 +1,19 @@
-import { useState, useEffect } from "react";
-import Link from "next/link";
 import axios from "axios";
-import { useRouter } from "next/router";
 import jwt_decode from "jwt-decode";
-import { useAuth } from "../../context/AuthContext";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useAuth } from "../../context/AuthContext";
+
 function Login({ handleLogPageToggle }) {
+
   const [data, setData] = useState({ email: "", password: "" });
   const [error, setError] = useState("");
   const [login, setLogin] = useState(false);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
   const router = useRouter();
   const {
     isMentorLoggedIn,
@@ -18,7 +22,12 @@ function Login({ handleLogPageToggle }) {
     setIsUserLoggedIn,
   } = useAuth();
 
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prevState) => !prevState);
+  };
+
   useEffect(() => {
+
     const handleCallBackResponse = async (response) => {
       const userObject = jwt_decode(response.credential);
       const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users/gloginauth`;
@@ -110,6 +119,7 @@ function Login({ handleLogPageToggle }) {
       toast.success("login successful");
     }
   };
+
   return (
     <>
       <form
@@ -135,7 +145,8 @@ function Login({ handleLogPageToggle }) {
           <h3 style={{ color: "black", alignSelf: "center", margin: "20px" }}>
             Or
           </h3>
-          <div className=" tw-flex tw-flex-col tw-mb-10">
+
+          <div className="tw-flex tw-flex-col tw-mb-10">
             <label
               for="name"
               className="tw-text-3xl tw-text-left tw-font-medium tw-mr-10"
@@ -148,24 +159,68 @@ function Login({ handleLogPageToggle }) {
               placeholder="Email"
               onChange={handleChange}
               value={data.email}
-              className=" tw-px-2 tw-border-b-[1px] tw-border-b-black tw-py-3 "
+              className="tw-px-2 tw-border-b-[1px] tw-border-b-black tw-py-3 "
             />
           </div>
-          <div className="tw-flex tw-flex-col tw-mb-10">
-            <label for="name" className="tw-text-3xl tw-font-medium ">
+
+          <div className="tw-flex tw-flex-col tw-mb-10 tw-relative">
+            <label for="name" className="tw-text-3xl tw-text-left tw-font-medium tw-mr-10"
+            >
               Password
             </label>
             <input
-              type="password"
+              type={isPasswordVisible ? 'text' : 'password'}
               name="password"
               placeholder="Password"
               onChange={handleChange}
               value={data.password}
-              className="tw-px-2 tw-border-b-[1px] tw-border-b-black tw-py-3 "
+              className="tw-px-2 tw-border-b-[1px] tw-border-b-black tw-py-3 tw-pr-16"
             />
+            <button
+              className="tw-absolute tw-inset-y-0 tw-right-0 tw-flex tw-items-center tw-px-4 tw-text-gray-600 tw-top-12"
+              onClick={togglePasswordVisibility}
+            >
+              {isPasswordVisible ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="tw-h-8 tw-w-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="tw-h-8 tw-w-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              )}
+            </button>
           </div>
+
           <div>
-            <input
+            <button
               type="submit"
               name="submit"
               value="Login"
@@ -175,7 +230,7 @@ function Login({ handleLogPageToggle }) {
               }}
               className="tw-px-10 tw-py-[6px] tw-font-semibold tw-text-white tw-rounded-3xl tw-cursor-pointer tw-w-full"
               onClick={addToast}
-            />
+            >Login</button>
           </div>
 
           <ToastContainer />

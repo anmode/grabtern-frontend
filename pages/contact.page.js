70dee69e3e254e20492709e569671451181f32d5
Geeta <geetkiroula1999@gmail.com>
2023-06-21 13:40:17 +0530

    Contact form validation checked before alert sent message issue#393 resolved

--- pages/contact.page.js
+++ pages/contact.page.js
@@ -1,17 +1,73 @@
-import React, { useRef } from "react";
+import React, { useRef ,useState} from "react";
 import emailjs from "@emailjs/browser";
 import dynamic from "next/dynamic";
 import style from "../styles/contact.module.css";
 import Head from "next/head";
+import { message } from "antd";
 
 const Footer = dynamic(() => import("../components/Footer"));
 const Header = dynamic(() => import("../components/Header"));
 const SimpleBanner = dynamic(() => import("../components/SimpleBanner"));
 
+
+
 function Contact() {
+
+  const [message, setMessage] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [error, seterror] = useState('');
+
   const form = useRef(null);
-  const sendEmail = (e) => {
-    e.preventDefault();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Do some authentication here...
+
+    //console.log("function called");
+
+    if(message === '')
+    { seterror("Enter any message!");}
+  
+    else if(name === '')
+    { seterror("Enter your Name!");}
+
+    else if(email === '')
+    { seterror("Enter your email!");}
+
+    else if(!validEmail(email))
+    { seterror("Enter valid email!");}
+
+    else if(subject === '')
+    { seterror("Enter a subject!");}
+
+    else
+    {
+      seterror("");
+      setMessage("");
+      setName("");
+      setEmail("");
+      setSubject("");
+
+      sendEmail();
+
+    }
+    
+  };
+
+  function validEmail(email)
+   {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if ( re.test(email) ) 
+    return true;
+    else
+    return false;
+   }
+
+  function sendEmail()
+  {
+   
     const templateParams = {
       from_name: document.querySelector("#name").value,
       to_name: document.querySelector("#email").value,
@@ -38,6 +94,7 @@ function Contact() {
       );
     form.current.reset();
   };
+
   return (
     <>
       <Head>
@@ -93,15 +150,23 @@ function Contact() {
                     className="form-contact contact_form"
                     id="contactForm"
                     ref={form}
-                    onSubmit={sendEmail}
+                    onSubmit={handleSubmit}
                   >
                     <div className={`${style.cf} row`}>
                       <h2 className={style.ctitle}>Get in Touch</h2>
 
+                       {
+                        error && (
+                          <p style={{color:"red",marginTop:"40px",marginLeft:"-235px",padding:"10px 0px"}}>{error}</p>
+                        ) 
+                      }
+       
+
                       <div className="col-12">
                         <div className={style.fgroup}>
                           <textarea
-                            className="form-control w-100"
+                          //className="form-control w-100"
+                          className={`form-control valid ${error === 'Enter any message!' && "inputField"}`}
                             name="message"
                             id="message"
                             cols={30}
@@ -110,15 +175,15 @@ function Contact() {
                             onblur="this.placeholder = 'Enter Message'"
                             placeholder=" Enter Message"
                             required
-                            // value={userMessage}
-                            // onChange={(e) => setUserMessage(e.value)}
+                            value={message}
+                            onChange={(event) => setMessage(event.target.value)}
                           />
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className={style.fgroup}>
                           <input
-                            className="form-control valid"
+                            className={`form-control valid ${error === 'Enter your Name!' && "inputField"}`}
                             name="name"
                             id="name"
                             type="text"
@@ -126,15 +191,15 @@ function Contact() {
                             onblur="this.placeholder = 'Enter your name'"
                             placeholder="Enter your name"
                             required
-                            // value={userName}
-                            // onChange={(e) => setUserName(e.value)}
+                            value={name}
+                            onChange={(event) => setName(event.target.value)}
                           />
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className={style.fgroup}>
                           <input
-                            className="form-control valid"
+                            className={`form-control valid ${error === 'Enter your email!' && "inputField"} ${error === 'Enter valid email!' && "inputField"}`}
                             name="email"
                             id="email"
                             type="email"
@@ -142,15 +207,15 @@ function Contact() {
                             onblur="this.placeholder = 'Enter email address'"
                             placeholder="Email"
                             required
-                            // value={userEmail}
-                            // onChange={(e) => setUserEmail(e.value)}
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
                           />
                         </div>
                       </div>
                       <div className="col-12">
                         <div className={style.fgroup}>
                           <input
-                            className="form-control"
+                            className={`form-control valid ${error === 'Enter a subject!' && "inputField"}`}
                             name="subject"
                             id="subject"
                             type="text"
@@ -158,13 +223,13 @@ function Contact() {
                             onblur="this.placeholder = 'Enter Subject'"
                             placeholder="Enter Subject"
                             required
-                            // value={userSubject}
-                            // onChange={(e) => setUserSubject(e.value)}
+                             value={subject}
+                            onChange={(event) => setSubject(event.target.value)}
                           />
                         </div>
                       </div>
                       <div>
-                        <button type="submit" className={style.boxedbtn}>
+                        <button type="submit" className={style.boxedbtn}  onClick={handleSubmit}>
                           Send
                         </button>
                       </div>

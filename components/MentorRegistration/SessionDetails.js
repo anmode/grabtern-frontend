e5033924f3b09b2bca5e88894dbe8ec0643a1074
isha988 <ishakapoor.988@gmail.com>
2023-06-21 11:10:47 +0530

    dayInput and timeZone Input created

--- components/MentorRegistration/SessionDetails.js
+++ components/MentorRegistration/SessionDetails.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import DayInput from "../dateAndTime/DayInput";
+import TimeZoneInput from "../dateAndTime/TimeZoneInput";
 import Input from "./Input";
 import ScheduleCard from "./ScheduleCard";
 
@@ -76,96 +78,26 @@ function SessionDetails({
         <label className="label uppercase" htmlFor="day">
           Day
         </label>
-        <select name="day" id="day" onChange={onChange} value={newSchedule.day}>
-          <option value="monday">Monday</option>
-          <option value="tuesday">Tuesday</option>
-          <option value="wednesday">Wednesday</option>
-          <option value="thursday">Thursday</option>
-          <option value="friday">Friday</option>
-          <option value="saturday">Saturday</option>
-          <option value="sunday">Sunday</option>
-        </select>
+        <DayInput
+          id="day"
+          name="day"
+          onChange={onChange}
+          value={newSchedule.value}
+        />
       </div>
       {/* day input ends */}
 
       {/* time zone input starts */}
       <div className="div">
-        <label htmlFor="timezone">Book session Date picker timezone</label>
-        <select
+        <label className="label uppercase" htmlFor="timezone">Time Zone</label>
+        <TimeZoneInput
           id="timezone"
           name="timezone"
           onChange={onChange}
           value={newSchedule.timezone}
-        >
-          <option value="(GMT-11:00) Pacific/Midway">
-            (GMT-11:00) Pacific/Midway
-          </option>
-          <option value="(GMT-11:00) Pacific/Niue">
-            (GMT-11:00) Pacific/Niue
-          </option>
-          <option value="(GMT-11:00) Pacific/Pago_Pago">
-            (GMT-11:00) Pacific/Pago_Pago
-          </option>
-          <option value="(GMT-10:00) Pacific/Honolulu">
-            (GMT-10:00) Pacific/Honolulu
-          </option>
-          <option value="(GMT-09:00) America/Anchorage">
-            (GMT-09:00) America/Anchorage
-          </option>
-          <option value="(GMT-08:00) America/Los_Angeles">
-            (GMT-08:00) America/Los_Angeles
-          </option>
-          <option value="(GMT-07:00) America/Denver">
-            (GMT-07:00) America/Denver
-          </option>
-          <option value="(GMT-06:00) America/Chicago">
-            (GMT-06:00) America/Chicago
-          </option>
-          <option value="(GMT-05:00) America/New_York">
-            (GMT-05:00) America/New_York
-          </option>
-          <option value="(GMT-04:00) America/Caracas">
-            (GMT-04:00) America/Caracas
-          </option>
-          <option value="(GMT-03:00) America/Buenos_Aires">
-            (GMT-03:00) America/Buenos_Aires
-          </option>
-          <option value="(GMT-02:00) Atlantic/South_Georgia">
-            (GMT-02:00) Atlantic/South_Georgia
-          </option>
-          <option value="(GMT-01:00) Atlantic/Azores">
-            (GMT-01:00) Atlantic/Azores
-          </option>
-          <option value="(GMT+00:00) Europe/London">
-            (GMT+00:00) Europe/London
-          </option>
-          <option value="(GMT+01:00) Europe/Paris">
-            (GMT+01:00) Europe/Paris
-          </option>
-          <option value="(GMT+02:00) Africa/Johannesburg">
-            (GMT+02:00) Africa/Johannesburg
-          </option>
-          <option value="(GMT+04:00) Asia/Dubai">(GMT+04:00) Asia/Dubai</option>
-          <option value="(GMT+05:30) Asia/Kolkata">
-            (GMT+05:30) Asia/Kolkata
-          </option>
-          <option value="(GMT+07:00) Asia/Jakarta">
-            (GMT+07:00) Asia/Jakarta
-          </option>
-          <option value="(GMT+09:00) Asia/Tokyo">(GMT+09:00) Asia/Tokyo</option>
-          <option value="(GMT+10:00) Australia/Sydney">
-            (GMT+10:00) Australia/Sydney
-          </option>
-          <option value="(GMT+11:00) Pacific/Noumea">
-            (GMT+11:00) Pacific/Noumea
-          </option>
-          <option value="(GMT+12:00) Pacific/Auckland">
-            (GMT+12:00) Pacific/Auckland
-          </option>
-        </select>
-        {/* time zone input ends */}
+        />
       </div>
-      {/* time zone input starts */}
+      {/* time zone input ends */}
 
       {/* time inputs start */}
       {timeInputs.map((input, index) => (

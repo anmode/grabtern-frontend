f17c1f71e0ae1e1d2bba161cc41d20d527a6e90e
Vidip Ghosh <91741581+Vidip-Ghosh@users.noreply.github.com>
2023-06-21 10:46:38 +0530

    New footer using tailwind css (#320)
    * Added Overlay.js file
    
    * New Footer Design
    
    * Changed footer color scheme
    
    * Updated Footer color
    
    * Added dividing line between categories
    
    * Added dividing line between categories
    
    * Updated color and added dividing lines
    
    * Input and Button in one line for smaller devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "tw-",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
+ 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
   theme: {
-    extend: {},
+    divideWidth:{
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    }
   },
   plugins: [],
-};
+}
+

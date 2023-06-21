fc971c0a833d4ec02e82d4b2b882733d3333d1f6
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-06-21 10:47:33 +0530

    fix the test

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,20 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    divideWidth:{
-      DEFAULT: '1px',
-      '0': '0',
-      '2': '2px',
-      '3': '3px',
-      '4': '4px',
-      '6': '6px',
-      '8': '8px',
-    }
+    divideWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
   },
   plugins: [],
-}
-
+};

8507e37b5c9d20c1b704cb8eb2c7f734fd1cd108
SVyusti <vyusti2083@gmail.com>
2023-06-24 02:08:55 +0530

    fix: cards ui

--- components/Internship.js
+++ components/Internship.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Image from "next/image";
-import {Card,Link, CardMedia, CardContent, CardActions, Button, CardActionArea, Typography} from '@mui/material'
+import {Image,Card,Text,Link} from "@nextui-org/react"
 
 
 function Internship({
@@ -24,40 +23,63 @@ function Internship({
     window.open(internshipLink, "_blank");
   };
   return (
-    
-    <Card
-      onClick={handleInternshipLinkClick}
-      className="properties__card"
+
+    <div
+      className="properties pb-20 item"
+      style={{ minHeight: "469px !important" }}
     >
-      <CardActionArea>
-          <CardMedia
+      <div className="properties__card">
+        <div>
+          <a href="#" onClick={handleInternshipLinkClick}>
+            <img
             className="properties__img"
-            component="img"
-            image={internshipImage}
+            src={internshipImage}
             onClick={handleInternshipLinkClick}
             alt={internshipImageAlt}
-          />
-      </CardActionArea>
-      <CardContent className="properties__info">
-        <Typography   
-          fontSize="16px" 
-          color="#6e7697"
-        >
-        {internshipCategories}
-        </Typography>
-      <CardActions>
-        <Link href={internshipLink}  className="properties__link">{internshipTitle}</Link>
-      </CardActions>
-        <div>
-        <Typography 
-          variant="p"  
-          fontSize="1.5rem" 
-          color="#6e7697"
-        >{internshipDescription}
-        </Typography>
+            />
+          </a>
         </div>
-      </CardContent>
-    </Card>
+        <div className="properties__info">
+          <div className="courseInfo">
+            <p>{internshipCategories}</p>
+            <h3>
+              <a href={internshipLink} className="properties__link" onClick={handleInternshipLinkClick}>
+                {internshipTitle}
+              </a>
+            </h3>
+            <p className="properties__Description">{internshipDescription}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    // <div >
+    // <Card
+    //   onClick={handleInternshipLinkClick}
+    //   className="properties__card"
+    // >
+    //   <Card.Footer>
+    //       <img
+    //         className="properties__img"
+    //         src={internshipImage}
+    //         onClick={handleInternshipLinkClick}
+    //         alt={internshipImageAlt}
+    //       />
+    //   </Card.Footer>
+    //   <Card.Body className="properties__info">
+    //     <Text  
+    //       fontSize="16px" 
+    //       color="#6e7697"
+    //       margin-bottom="2px"
+    //     >
+    //     {internshipCategories}
+    //     </Text>
+    //     <Link href={internshipLink}  className="properties__link">{internshipTitle}</Link>
+    //     <Text p className="properties__Description">
+    //       {internshipDescription}
+    //     </Text>
+    //   </Card.Body>
+    // </Card>
+    // </div>
   );
 }
 

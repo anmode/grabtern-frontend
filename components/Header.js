36f2c9b0772e2b89ddeba38cd3811137f2c5e153
Your Name <you@example.com>
2023-06-24 07:32:25 +0600

    updated header

--- components/Header.js
+++ components/Header.js
@@ -59,7 +59,13 @@ function Header() {
   }, []);
 
   return (
-    <nav className="tw-flex tw-justify-between tw-items-center tw-mb-0 tw-bg-gray-200 tw-fixed tw-w-full tw-z-20 tw-top-0 tw-left-0 tw-border-b tw-border-gray-400 tw-text-black">
+    <nav
+      className={
+        scrollY
+          ? "tw-flex tw-justify-between tw-items-center tw-mb-0 tw-bg-white tw-fixed tw-w-full tw-z-20 tw-top-0 tw-left-0 tw-text-black dark:tw-bg-muted-900 tw-shadow-lg tw-shadow-muted-300/30 dark:tw-shadow-muted-900/40 tw-transition-all tw-duration-300"
+          : "tw-flex tw-justify-between tw-items-center tw-mb-0 tw-bg-gray-200 tw-fixed tw-w-full tw-z-20 tw-top-0 tw-left-0 dark:tw-bg-muted-900 tw-shadow-lg tw-shadow-muted-300/30 dark:tw-shadow-muted-900/40 tw-text-black"
+      }
+    >
       <Link
         href="/"
         className="tw-flex tw-gap-2 tw-justify-center tw-items-center"
@@ -103,18 +109,18 @@ function Header() {
           {/* <DarkModeToggle /> */}
           <SunIcon className="tw-h-9 tw-bg-white tw-rounded-2xl tw-px-0.5 tw-w-9 tw-text-yellow-400 tw-cursor-pointer" />
 
-          <DropdownCard
+          {/* <DropdownCard
             isUserLoggedIn={isUserLoggedIn}
             isMentorLoggedIn={isMentorLoggedIn}
-          />
+          /> */}
 
-          {/* <button
+          <button
             type="button"
-            onClick=''
-            className="tw-text-white tw-mb-8 tw-bg-blue-700 hover:tw-bg-blue-600 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-white tw-font-medium tw-rounded-lg tw-text-md tw-px-3 tw-py-2 tw-text-center tw-mr-3 sm:tw-mb-0 md:tw-mr-5 dark:tw-bg-blue-700 dark:hover:tw-bg-blue-700 dark:focus:tw-ring-blue-100"
+            onClick=""
+            className="tw-text-white tw-mb-8 tw-bg-blue-700 hover:tw-bg-blue-600 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-white tw-font-medium tw-rounded-xl tw-text-md tw-px-3 tw-py-2 tw-text-center tw-mr-3 sm:tw-mb-0 md:tw-mr-5 dark:tw-bg-blue-700 dark:hover:tw-bg-blue-700 dark:focus:tw-ring-blue-100"
           >
-            Sign In
-          </button> */}
+            Sign Up
+          </button>
         </div>
       </div>
 
@@ -128,7 +134,7 @@ function Header() {
             }}
           />
           {toggleDropdown && (
-            <div className="tw-absolute tw-right-12 tw-top-full tw-mt-16 tw-w-full tw-p-6 tw-rounded-lg tw-bg-white dark:tw-bg-slate-600 dark:tw-text-white tw-min-w-[200px] tw-flex tw-flex-col tw-gap-5 tw-justify-end tw-items-center">
+            <div className="tw-absolute tw-right-0 tw-top-full tw-mt-6 tw-w-full tw-p-6 tw-rounded-lg tw-bg-white dark:tw-bg-slate-600 dark:tw-text-white tw-min-w-[320px] tw-flex tw-flex-col tw-gap-6 tw-justify-start tw-items-center tw-text-xl tw-font-semibold">
               <Link
                 href="/"
                 className="tw-text-xl tw-p-2 tw-font-inter tw-text-gray-700 hover:tw-text-gray-500 tw-font-medium"
@@ -148,14 +154,24 @@ function Header() {
                 Contact
               </Link>
 
-              <Link href="/" className="hover:tw-text-blue-800">
+              <Link
+                href="/"
+                className="tw-text-xl tw-p-2 tw-font-inter tw-text-gray-700 hover:tw-text-gray-500 tw-font-medium"
+              >
                 Sign In
               </Link>
+              <button
+                type="button"
+                onClick=""
+                className="tw-w-1/2 tw-rounded-full tw-border tw-border-blue-600 tw-bg-blue-600 tw-py-2 tw-px-5 tw-text-white tw-transition-all hover:tw-bg-white hover:tw-text-black tw-text-center tw-text-md tw-font-inter tw-flex tw-items-center tw-justify-center"
+              >
+                Sign Up
+              </button>
 
-              <DropdownCard
+              {/* <DropdownCard
                 isUserLoggedIn={isUserLoggedIn}
                 isMentorLoggedIn={isMentorLoggedIn}
-              />
+              /> */}
             </div>
           )}
         </div>

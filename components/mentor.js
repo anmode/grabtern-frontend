20de9fb3ceeb7a73f979218edf417b7d6f7a785f
Vidip Ghosh <vidip2001@gmail.com>
2023-06-25 11:11:52 +0530

    Reduce height

--- components/mentor.js
+++ components/mentor.js
@@ -3,17 +3,16 @@ import React from "react";
 const MentorCard = ({ mentor }) => {
   return (
         <div className="card-container tw-flex flex-lg-column align-center tw-rounded-2xl tw-gap-5">
-         <div className="upper-container tw-bg-[#845ec2] tw-h-1/3 tw-w-full tw-rounded-t-2xl">
+         <div className="upper-container tw-bg-[#7c4dff] tw-h-1/3 tw-w-full tw-rounded-t-2xl">
             <div className="tw-w-full tw-h-full d-flex justify-content-center position-relative tw-top-20">
                <img className='tw-w-48 tw-h-48 tw-rounded-full' src={mentor.mentorImg} alt={mentor.name}/>
             </div>
          </div>
             <div className="lower-container tw-h-2/3 tw-bg-white tw-rounded-2xl">
-              <h1 className="tw-font-bold tw-text-4xl tw-text-black text-center tw-py-14">{mentor.name}</h1>
+              <h1 className="tw-font-bold tw-text-4xl text-center tw-py-12">{mentor.name}</h1>
               <div className="tw-flex tw-justify-center">
-                <p className="tw-text-center tw-font-semibold tw-text-3xl pb-5 tw-text-neutral-600">
-                  <b>Intern at: </b>
-                    {mentor.internAt}
+                <p className="tw-text-center tw-font-medium tw-text-3xl">
+                  Interned at: {mentor.internAt}
                 </p>
               </div>
           </div>

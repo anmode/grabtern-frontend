74c6c6c55802210813f538b174a37d09d7038dcc
EPSA <120899038+ePSA-eJya@users.noreply.github.com>
2023-06-27 18:15:42 +0530

    Update Testimonial.js

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Image from "next/image";
 
+// $(document).ready(function(){
+//   ('.owl-carousel').owlCarousel(
+//       nav=true,
+//       responsive={})
+//    ( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+//    ( ".owl-next").html('<i class="fa fa-chevron-right"></i>');
+//   });
+
 function Testimonial({
   testimonialUserName,
   testimonialUserHeadline,
@@ -12,7 +20,17 @@ function Testimonial({
     <div className="box tw-flex tw-flex-col-reverse tw-border-dashed tw-shadow-2xl tw-p-5">
       
       <div className="content">
-        
+      <div className="image">
+          {
+            <Image
+              width={75}
+              height={75}
+              className="tw-w-4 tw-h-auto"
+              src={testimonialUserImage}
+              alt="testimonial-profile"
+            />
+          }
+        </div>
         <div className="info">
           <div className="name tw-font-bold">{testimonialUserName}</div>
           <div className="job tw-font-semibold">{testimonialUserHeadline}</div>
@@ -28,23 +46,14 @@ function Testimonial({
             ) : null}
           </div>
         </div>
-        <div className="image">
-          {
-            <Image
-              width={75}
-              height={75}
-              className="tw-w-4 tw-h-auto"
-              src={testimonialUserImage}
-              alt="testimonial-profile"
-            />
-          }
-        </div>
+        
       </div>
-      <p>
+      <div className="description"><p>
         {testimonialDescription.split(" ").length < 100
           ? testimonialDescription
           : testimonialDescription.split(" ").slice(0, 100).join(" ") + "..."}
       </p>
+      </div>
       <i className="fas fa-quote-left quote"></i>
     </div>
   );

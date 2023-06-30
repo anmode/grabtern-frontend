d87f324196611553a140c56987e1412b5de7dbc4
Shreya Dhiman <shreyanksha2004@gmail.com>
2023-07-01 03:03:05 +0530

    responsive

--- pages/404.page.js
+++ pages/404.page.js
@@ -2,33 +2,44 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/404.module.css";
+import dynamic from "next/dynamic";
+const Footer = dynamic(() => import("../components/Footer"));
+const Header = dynamic(() => import("../components/Header"));
+const SimpleBanner = dynamic(() => import("../components/SimpleBanner"));
 
 function ErrorPage() {
     return (
         <>
-            <div className={styles.imageContainer} >
-                <img
-                    src="/Errorr.svg"
-                    alt="bg"
-                />
-            </div>
-            <div className={`${styles.mainSection}`}>
-                <div className={styles.text1}>
-                    <div className={styles.text2}>
-                        <h1>Error page 404</h1>
-                        <p>Lorem ipsum dolor sit amet. Est voluptas eaque vel nihil fugiat aut nemo aspernatur id similique repudiandae et laborum voluptatibus? Ea tenetur illo est ipsum possimus eos velit veritatis est deleniti illum quo velit neque ut enim laborum.</p>
-                    </div>
-                    <button className="md:tw-w-auto tw-h-16 tw-text-white tw-bg-[#845ec2] tw-border-0 tw-py-2 tw-px-6 focus:tw-outline-none hover:tw-bg-[#6b21a8] tw-rounded-lg tw-font-semibold">
-                        <Link href="/">Go Back</Link>
-                    </button>
-                </div>
-                <div className={styles.image2} >
+            <Header />
+            <SimpleBanner bannerTittle="Error 404" siteName="Error" />
+            <div className={styles.mainContainer} >
+                <div className={styles.imageContainer} >
                     <img
-                        src="/404.svg"
-                        alt="404"
+                        src="/error.png"
+                        alt="bg"
                     />
                 </div>
+                <div className={`${styles.mainSection}`}>
+                    <div className={styles.text1}>
+                        <div className={styles.text2}>
+                            <h1>Error page 404</h1>
+                            <p>Lorem ipsum dolor sit amet. Est voluptas eaque vel nihil fugiat aut nemo aspernatur id similique repudiandae et laborum voluptatibus? Ea tenetur illo est ipsum possimus eos velit veritatis est deleniti illum quo velit neque ut enim laborum.</p>
+                        </div>
+                        <div className={styles.btn}>
+                            <button className="md:tw-w-auto tw-h-16 tw-text-white tw-bg-[#845ec2] tw-border-0 tw-py-2 tw-px-6 focus:tw-outline-none hover:tw-bg-[#6b21a8] tw-rounded-lg tw-font-semibold">
+                                <Link href="/">Go Back</Link>
+                            </button>
+                        </div>
+                    </div>
+                    <div className={styles.image2} >
+                        <img
+                            src="/404.svg"
+                            alt="404"
+                        />
+                    </div>
+                </div>
             </div>
+            <Footer />
         </>
 
     );

668e03d0dd4d88cab484281395c765af15d8426f
isha988 <ishakapoor.988@gmail.com>
2023-07-02 08:58:21 +0530

    button component created

---
+++ components/UI/Button/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from './Button.module.css'
+
+function Button({ text, variant = "primary", className, type="button", onClick}) {
+  return (
+    <button
+      className={`${styles.btn} ${
+        className ? className : ""
+      }`}
+      onClick={onClick}
+      type={type}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Button;

bf59f9dc96859dee1e730f75ef3605514024967c
isha988 <ishakapoor.988@gmail.com>
2023-07-02 09:00:58 +0530

    pritter command

--- components/UI/Button/Button.js
+++ components/UI/Button/Button.js
@@ -1,12 +1,16 @@
 import React from "react";
-import styles from './Button.module.css'
+import styles from "./Button.module.css";
 
-function Button({ text, variant = "primary", className, type="button", onClick}) {
+function Button({
+  text,
+  variant = "primary",
+  className,
+  type = "button",
+  onClick,
+}) {
   return (
     <button
-      className={`${styles.btn} ${
-        className ? className : ""
-      }`}
+      className={`${styles.btn} ${className ? className : ""}`}
       onClick={onClick}
       type={type}
     >
--- pages/components.page.js
+++ pages/components.page.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Button from '../components/UI/Button/Button'
+import React from "react";
+import Button from "../components/UI/Button/Button";
 
 function components() {
   return (
     <div>
-      Primary: <Button text="click here" onClick={()=>{}}/>
+      Primary: <Button text="click here" onClick={() => {}} />
     </div>
-  )
+  );
 }
 
-export default components
+export default components;

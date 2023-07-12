e9e61265c88a44c74bce34e444694d69cd4a663a
Isha Kapoor <89572340+Isha988@users.noreply.github.com>
2023-07-12 17:23:05 +0530

    Refactoring 4 (#498)
    * added row reverse direction in sction component
    
    * fix an error
    
    * added shadow, xl size and fixed height in profile card and updated mentor.js
    
    * mentor profile page about section refactored
    
    * prettier command
    
    * prettier command
    
    * removed static image url
    
    * improved session section header
    
    * session cards
    
    * prettier formatting
    
    * testimonial cards
    
    * delete extra file
    
    * removed static url
    
    * dynamic icon for type of session
    
    * share button function added

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,50 +1,44 @@
 import React from "react";
-import Image from "next/image";
+import { FaQuoteLeft } from "react-icons/fa";
+import { IconCard, ProfileCard } from "./UI";
 
 function Testimonial({
   testimonialUserName,
   testimonialUserHeadline,
   testimonialUserImage,
   testimonialRate,
+  testimonialHeadline,
   testimonialDescription,
 }) {
   return (
-    <div className="box tw-flex tw-flex-col-reverse tw-border-dashed tw-shadow-2xl tw-p-5">
-      <p>
-        {testimonialDescription.split(" ").length < 100
-          ? testimonialDescription
-          : testimonialDescription.split(" ").slice(0, 100).join(" ") + "..."}
-      </p>
-      <div className="content">
-        <div className="image">
-          {
-            <Image
-              width={75}
-              height={75}
-              className="tw-w-4 tw-h-auto"
-              src={testimonialUserImage}
-              alt="testimonial-profile"
-            />
-          }
-        </div>
-        <div className="info">
-          <div className="name tw-font-bold">{testimonialUserName}</div>
-          <div className="job tw-font-semibold">{testimonialUserHeadline}</div>
-          <div className="stars">
-            {Array(Math.floor(testimonialRate))
-              .fill("")
-              .map((_, index) => (
-                <i key={index} className="fas fa-star"></i>
-              ))}
+    <IconCard
+      Icon={FaQuoteLeft}
+      heading={testimonialHeadline}
+      body={testimonialDescription}
+      intent="bg"
+      size="lg"
+    >
+      {/* user details */}
+      <ProfileCard
+        className="tw-mt-4"
+        image={testimonialUserImage}
+        heading={testimonialUserName}
+        subheading={testimonialUserHeadline}
+        size="sm"
+      />
+      {/* rating */}
+      <div className="tw-text-yellow-400 tw-ml-12 -tw-mt-2">
+        {Array(Math.floor(testimonialRate))
+          .fill("")
+          .map((_, index) => (
+            <i key={index} className="fas fa-star"></i>
+          ))}
 
-            {testimonialRate.toString().includes(".") ? (
-              <i className="fas fa-star-half"></i>
-            ) : null}
-          </div>
-        </div>
+        {testimonialRate.toString().includes(".") ? (
+          <i className="fas fa-star-half"></i>
+        ) : null}
       </div>
-      <i className="fas fa-quote-left quote"></i>
-    </div>
+    </IconCard>
   );
 }
 

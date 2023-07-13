19457837d0d1b2443496ba5d54fe9b9d598b84fc
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-07-13 23:50:57 +0530

    fix: Changes Navbar styling 🎉

--- pages/internship.page.js
+++ pages/internship.page.js
@@ -1,5 +1,5 @@
 import internshipsData from "./data/coursesData";
-
+import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 
 import dynamic from "next/dynamic";
@@ -258,85 +258,8 @@ export default function Home() {
           />
         </div>
       ) : null}
+      <Header isUserLoggedIn={isUserLoggedIn} />
 
-      <div className="header-area header-transparent tw-z-[999]">
-        <div className="main-header ">
-          <div
-            className={`header-bottom  header-sticky ${
-              scrollY > 400
-                ? "sticky-bar"
-                : navbarBackground === true
-                ? "sticky-bar"
-                : ""
-            }`}
-            style={{ transition: "all 0.5s ease-in" }}
-          >
-            <div className="container-fluid">
-              <div className="row align-items-center justify-content-between">
-                <div>
-                  <div className="logo">
-                    <a href="/">
-                      <Image
-                        width={80}
-                        height={80}
-                        src="/whitelogo.webp"
-                        style={{ padding: "15px 0" }}
-                        alt="grabtern_logo"
-                      />
-                    </a>
-                  </div>
-                </div>
-                <div className="col-xl-10 col-lg-10">
-                  <div className="menu-wrapper d-flex align-items-center justify-content-end">
-                    <div
-                      className={`main-menu d-none d-lg-block ${
-                        navbarAppear === true ? "active" : ""
-                      }`}
-                    >
-                      <nav>
-                        <ul id="navigation" className="navigation">
-                          <li className="active">
-                            <a href="/" className={styles.navLink}>
-                              Home
-                            </a>
-                          </li>
-                          <li>
-                            <a href="/mentors" className={styles.navLink}>
-                              Mentors
-                            </a>
-                          </li>
-                          <li>
-                            <a href="/contact" className={styles.navLink}>
-                              Contact
-                            </a>
-                          </li>
-                          <DropdownCard
-                            isUserLoggedIn={isUserLoggedIn}
-                            isMentorLoggedIn={isMentorLoggedIn}
-                          />
-                        </ul>
-                      </nav>
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className={`menuToggle ${
-                    navbarAppear === true ? "active" : ""
-                  }`}
-                  onClick={() => menuToggle()}
-                >
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-                <div className="col-12">
-                  <div className="mobile_menu d-block d-lg-none"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <main>
         <div className={`${hackathonStyle.hackathonArea} section-padding40`}>
           <div className="container">

1fdc77ad255ee5c4b0477bb62ce97527d7b43505
isha988 <ishakapoor.988@gmail.com>
2023-07-15 16:04:52 +0530

    improved service card

--- components/homePage/Service.js
+++ components/homePage/Service.js
@@ -1,20 +1,16 @@
 import React from "react";
-import Image from "next/image";
+import { IconCard } from "../UI";
 
-function Service({ imageSrc, imageAlt, serviceHeading, serviceDescription }) {
-  console.log(imageSrc, imageAlt, serviceHeading, serviceDescription);
+function Service({ ServiceIcon, serviceHeading, serviceDescription }) {
   return (
-    <div className="">
-      <div className="tw-flex tw-shadow-md tw-rounded-lg tw-bg-base-100 tw-py-6 tw-px-5 tw-h-full">
-        <div className="tw-mt-3">
-          <Image width={60} height={60} src={imageSrc} alt={imageAlt} />
-        </div>
-        <div className="tw-pl-5">
-          <h3 className="tw-text-xl tw-font-medium">{serviceHeading}</h3>
-          <p>{serviceDescription}</p>
-        </div>
-      </div>
-    </div>
+    <IconCard
+      className="tw-shadow-sm"
+      Icon={Object.values(ServiceIcon)[0]}
+      heading={serviceHeading}
+      body={serviceDescription}
+      intent="bg"
+      size="lg"
+    />
   );
 }
 

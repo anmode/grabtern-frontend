36372ea46ec3a678e0bcc32e69efb124f7332570
isha988 <ishakapoor.988@gmail.com>
2023-07-15 13:16:00 +0530

    revert services data file  changes

--- pages/data/ServicesData.js
+++ pages/data/ServicesData.js
@@ -1,23 +1,21 @@
-import {
-  PiShootingStarDuotone,
-  PiGraduationCapDuotone,
-  PiAlarmDuotone,
-} from "react-icons/pi";
 export default [
   {
-    ServiceIcon: { PiShootingStarDuotone },
+    imageSrc: "/assets/img/icon/icon1.svg",
+    imageAlt: "40+ Dream Internships",
     serviceHeading: "40+ Dream Internships",
     serviceDescription:
       "Grabtern Contains the dream Internship and global hackathons.",
   },
   {
-    ServiceIcon: { PiGraduationCapDuotone },
+    imageSrc: "/assets/img/icon/icon2.svg",
+    imageAlt: "Former Interns",
     serviceHeading: "Former Interns",
     serviceDescription:
       "Get the mentorship from the past fellows and increase your chance to get your internship",
   },
   {
-    ServiceIcon: { PiAlarmDuotone },
+    imageSrc: "/assets/img/icon/icon3.svg",
+    imageAlt: "Book your Session",
     serviceHeading: "Book your Session",
     serviceDescription:
       "Session are at very minimal price, you can book multiple session to multiple interns",

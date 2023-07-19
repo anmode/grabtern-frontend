28cfdad7f7bafc214cab78a69a03ae8095098437
Shraddha Singh <shraddhasinghbais4@gmail.com>
2023-07-19 15:38:28 +0530

    Register page is designed

--- pages/register.page.js
+++ pages/register.page.js
@@ -2,15 +2,55 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-import Visibillity from "../../public/assets/Visibillity.jsx";
-import VisibillityOff from "../../public/assets/VisibillityOff.jsx";
-import { encryptData, decryptData } from "../../hook/encryptDecrypt.js";
+import Visibillity from "../public/assets/Visibillity.jsx";
+import VisibillityOff from "../public/assets/VisibillityOff.jsx";
 
 function useRedirectIfAuthenticated() {
   const router = useRouter();
 
   useEffect(() => {
-    const userDataString = decryptData(localStorage.getItem("userData"));
+    const handleCallBackResponse = async (response) => {
+      const userObject = jwt_decode(response.credential);
+      const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users/gloginauth`;
+      try {
+        const res = await axios.post(url, userObject);
+        console.log(res);
+        const userData = {
+          user_name: userObject.name,
+          user_picture: userObject.picture,
+          user_email: userObject.email,
+          user_id: res.data.id,
+        };
+
+        localStorage.setItem("userData", encryptData(userData));
+
+        const redirectUrl = sessionStorage.getItem("redirectUrl") || "/";
+        router.push(redirectUrl);
+      } catch (error) {
+        setError("New user? Register first.");
+        console.log(error);
+      }
+    };
+
+    const initGoogleSignIn = () => {
+      try {
+        google.accounts.id.initialize({
+          client_id:
+            "1094459761-kbb3qbgafu8avkgfe9fk8f85fr5418a8.apps.googleusercontent.com",
+          callback: handleCallBackResponse,
+        });
+        google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+          theme: "outline",
+          size: "large",
+        });
+        google.accounts.id.prompt();
+      } catch (error) {
+        console.error("Google sign-in initialization failed:", error);
+      }
+    };
+
+    initGoogleSignIn();
+    const userDataString = localStorage.getItem("userData");
     if (userDataString) {
       const userData = JSON.parse(userDataString);
       if (userData.name !== null || userData.token !== null) {
@@ -19,6 +59,8 @@ function useRedirectIfAuthenticated() {
     }
   }, [router]);
 }
+
+
 
 function Register({ handleLogPageToggle }) {
   useRedirectIfAuthenticated();
@@ -47,6 +89,8 @@ function Register({ handleLogPageToggle }) {
     setData({ ...data, [input.name]: input.value });
   };
 
+  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
@@ -72,25 +116,42 @@ function Register({ handleLogPageToggle }) {
     }
   };
   return (
-    <>
+    <div 
+    style={{
+      display: "flex",
+      alignContent: "center",
+      justifyContent: "center",
+    }}
+    >
       <form
         className="form-default mx-5"
         action="login-bg.mp4"
         onSubmit={handleSubmit}
         style={{ marginTop: "10vh" }}
-        aria-label="User registration form"
       >
         <div className="d-flex flex-column justify-content-start tw-w-full  md:tw-py-[5vh] md:tw-px-[3vw]  tw-py-[4vh] tw-px-[5vw] tw-shadow-2xl">
-          <h2 className="text-left tw-text-black tw-text-5xl  tw-font-bold">
-            Registration
+        <div style={{ marginBottom: "20px" }}>
+          <h2 className="text-left tw-text-black tw-text-4xl  tw-font-bold">
+          Hey, hello 👋
           </h2>
-          <p className=" tw-text-gray-500 tw-mb-5">
-            The faster you fill up, the faster you get a internship
+          <p className=" tw-text-gray-800 tw-mb-6 tw-mt-3">
+          The faster you fill up, the faster you get a internship
           </p>
-          <div className="tw-flex tw-flex-col  tw-mb-10">
+          </div>
+          <div
+            id="signInDiv"
+            style={{ alignSelf: "center" }}
+            className="tw-mb-2 top: -10px;"
+          > </div>
+          <h3 style={{ color: "black", alignSelf: "center", margin: "20px" }}>
+            Or
+          </h3>
+
+          <div className="tw-flex tw-flex-col  tw-mb-10"  >
             <label
-              htmlFor="name"
-              className="tw-text-3xl tw-text-left tw-font-medium "
+              for="name"
+              className="tw-text-lg tw-text-left tw-font-small "
+              style={{ fontSize: '1.3em' }}
             >
               Full name
             </label>
@@ -98,56 +159,48 @@ function Register({ handleLogPageToggle }) {
               type="text"
               placeholder="Full name"
               name="fullName"
-              id="fullName"
               onChange={handleChange}
               value={data.fullName}
               className="tw-px-2 tw-border-b-[1px] tw-border-b-black tw-py-3 "
-              required
-              aria-required="true"
             />
           </div>
           <div className="tw-flex tw-flex-col  tw-mb-10">
             <label
-              htmlFor="email"
+              for="name"
               className="tw-text-3xl tw-text-left tw-font-medium tw-mr-10"
+              style={{ fontSize: '1.3em' }}
             >
               Email Address
             </label>
             <input
               type="email"
               name="email"
-              id="email"
               placeholder="Email Address"
               onChange={handleChange}
               value={data.email}
               className="tw-px-2 tw-border-b-[1px] tw-border-b-black tw-py-3 "
-              required
-              aria-required="true"
             />
           </div>
 
           <div className="tw-flex tw-flex-col tw-mb-10 tw-relative">
             <label
-              htmlFor="password"
+              for="name"
               className="tw-text-3xl tw-text-left tw-font-medium tw-mr-10"
+              style={{ fontSize: '1.3em' }}
             >
               Password
             </label>
             <input
               type={isPasswordVisible ? "text" : "password"}
               name="password"
-              id="password"
               placeholder="Password"
               onChange={handleChange}
               value={data.password}
               className="tw-px-2 tw-border-b-[1px] tw-border-b-black tw-py-3 tw-pr-16"
-              required
-              aria-required="true"
             />
             <div
               className="tw-absolute tw-inset-y-0 tw-right-0 tw-flex tw-px-4 tw-text-gray-600 tw-top-16"
               onClick={togglePasswordVisibility}
-              aria-label={isPasswordVisible ? "Hide Password" : "Show Password"}
             >
               {isPasswordVisible ? <VisibillityOff /> : <Visibillity />}
             </div>
@@ -155,21 +208,19 @@ function Register({ handleLogPageToggle }) {
 
           <div className="tw-flex tw-flex-col tw-mb-10 tw-relative">
             <label
-              htmlFor="confirmPassword"
+              for="name"
               className="tw-text-3xl tw-text-left tw-font-medium tw-mr-10"
+              style={{ fontSize: '1.3em' }}
             >
               Confirm Password
             </label>
             <input
               type={isConPasswordVisible ? "text" : "password"}
               name="confirmPassword"
-              id="confirmPassword"
               placeholder="Confirm Password"
               onChange={handleChange}
               value={data.confirmPassword}
               className="tw-px-2 tw-border-b-[1px] tw-border-b-black tw-py-3 tw-pr-16"
-              required
-              aria-required="true"
             />
             <div
               className="tw-absolute tw-inset-y-0 tw-right-0 tw-flex tw-px-4 tw-text-gray-600 tw-top-16"
@@ -184,11 +235,7 @@ function Register({ handleLogPageToggle }) {
               verify your account.
             </p>
           )}
-          {error && (
-            <div style={{ color: "red" }} role="alert">
-              {error}
-            </div>
-          )}
+          {error && <div style={{ color: "red" }}>{error}</div>}
           <div>
             <input
               type="submit"
@@ -213,7 +260,7 @@ function Register({ handleLogPageToggle }) {
           </div>
         </div>
       </form>
-    </>
+    </div>
   );
 }
 

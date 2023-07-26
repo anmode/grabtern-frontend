4484338dd241147baa255b1aef873b28d20c1ce8
Vidip Ghosh <vidip2001@gmail.com>
2023-07-26 10:38:49 +0530

    Hover color of table links

--- pages/privacy.page.js
+++ pages/privacy.page.js
@@ -29,7 +29,7 @@ function privacy() {
             when you use our services ("
             <b className="tw-text-[#845ec2]">Services</b>"), such as when you:
             Visit our website at{" "}
-            <a href="https://www.grabtern.com">https://www.grabtern.com</a>, or
+            <a className="hover:tw-text-[#5C18E1]" href="https://www.grabtern.com">https://www.grabtern.com</a>, or
             any website of ours that links to this privacy notice Engage with us
             in other related ways, including any sales, marketing, or events.
             <b className="tw-text-[#845ec2]"></b>
@@ -46,13 +46,13 @@ function privacy() {
             This summary provides key points from our privacy notice, but you
             can find out more details about any of these topics by clicking the
             link following each key point or by using our{" "}
-            <a href="#toc">table of contents</a> below to find the section you
+            <a className="hover:tw-text-[#5C18E1]" href="#toc">table of contents</a> below to find the section you
             are looking for.<br></br>
             What personal information do we process? When you visit, use, or
             navigate our Services, we may process personal information depending
             on how you interact with Grabtern and the Services, the choices you
             make, and the products and features you use.{" "}
-            <a href="#1">
+            <a className="hover:tw-text-[#5C18E1]" href="#1">
               Learn more about personal information you disclose to us.
             </a>
             <br></br>
@@ -68,12 +68,12 @@ function privacy() {
             also process your information for other purposes with your consent.
             We process your information only when we have a valid legal reason
             to do so.{" "}
-            <a href="#2">Learn more about how we process your information.</a>
+            <a className="hover:tw-text-[#5C18E1]" href="#2">Learn more about how we process your information.</a>
             <br></br>
             In what situations and with which parties do we share personal
             information? We may share information in specific situations and
             with specific third parties.{" "}
-            <a href="#3">
+            <a className="hover:tw-text-[#5C18E1]" href="#3">
               Learn more about when and with whom we share your personal
               information.
             </a>
@@ -86,16 +86,16 @@ function privacy() {
             cybercriminals, or other unauthorized third parties will not be able
             to defeat our security and improperly collect, access, steal, or
             modify your information.{" "}
-            <a href="#8">Learn more about how we keep your information safe.</a>
+            <a className="hover:tw-text-[#5C18E1]" href="#8">Learn more about how we keep your information safe.</a>
             <br></br>
             What are your rights? Depending on where you are located
             geographically, the applicable privacy law may mean you have certain
             rights regarding your personal information.{" "}
-            <a href="#8">Learn more about your privacy rights.</a>
+            <a className="hover:tw-text-[#5C18E1]" href="#8">Learn more about your privacy rights.</a>
             <br id="1"></br>
             How do you exercise your rights? The easiest way to exercise your
             rights is by submitting a{" "}
-            <a href="https://app.termly.io/notify/c64101d9-760d-43fe-89df-0884d72c12d9">
+            <a className="hover:tw-text-[#5C18E1]" href="https://app.termly.io/notify/c64101d9-760d-43fe-89df-0884d72c12d9">
               data subject access request
             </a>
             , or by contacting us. We will consider and act upon any request in
@@ -103,7 +103,7 @@ function privacy() {
             <br></br>
             Want to learn more about what Grabtern does with any information we
             collect?{" "}
-            <a href="#tncheading">Review the privacy notice in full.</a>
+            <a className="hover:tw-text-[#5C18E1]" href="#tncheading">Review the privacy notice in full.</a>
           </div>
           <div className="tableofcontents tw-scroll-smooth">
             <div className="tnc" id="toc">
@@ -111,49 +111,67 @@ function privacy() {
             </div>
             <div style={{ paddingRight: 10, paddingTop: 10 }}>
               <div>
-                <a href="#1">1. WHAT INFORMATION DO WE COLLECT?</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#1">
+                  1. WHAT INFORMATION DO WE COLLECT?
+                </a>
               </div>
               <div>
-                <a href="#2">2. HOW DO WE PROCESS YOUR INFORMATION?</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#2">
+                  2. HOW DO WE PROCESS YOUR INFORMATION?
+                </a>
               </div>
               <div>
-                <a href="#3">
+                <a className="hover:tw-text-[#5C18E1]" href="#3">
                   3. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
                 </a>
               </div>
               <div>
-                <a href="#4">
+                <a className="hover:tw-text-[#5C18E1]" href="#4">
                   4. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?
                 </a>
               </div>
               <div>
-                <a href="#5">5. HOW DO WE HANDLE YOUR SOCIAL LOGINS?</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#5">
+                  5. HOW DO WE HANDLE YOUR SOCIAL LOGINS?
+                </a>
               </div>
               <div>
-                <a href="#6">6. HOW LONG DO WE KEEP YOUR INFORMATION?</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#6">
+                  6. HOW LONG DO WE KEEP YOUR INFORMATION?
+                </a>
               </div>
               <div>
-                <a href="#7">7. HOW DO WE KEEP YOUR INFORMATION SAFE?</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#7">
+                  7. HOW DO WE KEEP YOUR INFORMATION SAFE?
+                </a>
               </div>
               <div>
-                <a href="#8">8. WHAT ARE YOUR PRIVACY RIGHTS?</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#8">
+                  8. WHAT ARE YOUR PRIVACY RIGHTS?
+                </a>
               </div>
               <div>
-                <a href="#9">9. CONTROLS FOR DO-NOT-TRACK FEATURES</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#9">
+                  9. CONTROLS FOR DO-NOT-TRACK FEATURES
+                </a>
               </div>
               <div>
-                <a href="#10">
+                <a className="hover:tw-text-[#5C18E1]" href="#10">
                   10. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?
                 </a>
               </div>
               <div>
-                <a href="#11">11. DO WE MAKE UPDATES TO THIS NOTICE?</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#11">
+                  11. DO WE MAKE UPDATES TO THIS NOTICE?
+                </a>
               </div>
               <div>
-                <a href="#12">12. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</a>
+                <a className="hover:tw-text-[#5C18E1]" href="#12">
+                  12. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?
+                </a>
               </div>
               <div>
-                <a href="#13">
+                <a className="hover:tw-text-[#5C18E1]" href="#13">
                   13. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT
                   FROM YOU?
                 </a>
@@ -161,7 +179,7 @@ function privacy() {
             </div>
           </div>
         </div>
-        <div className="terms-and-cond tw-rounded-3xl tw-container tw-mt-16 tw-shadow-2xl tw-p-5">
+        <div className="terms-and-cond tw-rounded-3xl tw-container tw-mt-16 tw-shadow-2xl tw-p-16">
           <div className="terms-and-cond-sub">
             <div className="tnc-sub-head">
               1. WHAT INFORMATION DO WE COLLECT?

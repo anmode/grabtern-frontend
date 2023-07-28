c52230b6ff54682e1d4ba7f19f722bf17894c674
Anmol Agrawal <88332977+anmode@users.noreply.github.com>
2023-07-28 19:36:02 +0530

    Revert "Rename AddIntership to AddInternship (#618)" (#619)
    This reverts commit 4d1c55f6bd1077d3f7cd052fa61ae3ba4d47adc9.

--- components/AddIntership.js
+++ components/AddIntership.js
@@ -20,7 +20,7 @@ const options = InternshipLabels.map((label) => {
   return { value: label, label: label };
 });
 
-const AddInternship = ({ handleShow }) => {
+const AddIntership = ({ handleShow }) => {
   const [formData, setFormData] = React.useState({
     internshipImage: "",
     internshipImageAlt: "",
@@ -287,4 +287,4 @@ const AddInternship = ({ handleShow }) => {
   );
 };
 
-export default AddInternship;
+export default AddIntership;

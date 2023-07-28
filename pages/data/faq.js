29f6d9baa8a969ac1a52da1cdcff66243526367c
Rishu Raj <rrishu561@gmail.com>
2023-07-29 00:21:44 +0530

    feat: testimonials section (#617)
    * add: testimonials section
    
    * add: more faqs

--- pages/data/faq.js
+++ pages/data/faq.js
@@ -4,19 +4,27 @@ export default [
     ans: "A mentor is a person who can support, advise and guide you. They typically take the time to get to know you and the challenges you’re facing and then use their understanding and personal experience to help you improve.",
   },
   {
-    ques: "Who is a mentor?",
-    ans: "A mentor is a person who can support, advise and guide you. They typically take the time to get to know you and the challenges you’re facing and then use their understanding and personal experience to help you improve.",
+    ques: "Can I be a mentor in multiple areas or subjects?",
+    ans: "Yes, if you have expertise and experience in multiple areas, you can choose to mentor in more than one field. However, it's essential to ensure that you can dedicate enough time and attention to each mentee.",
   },
   {
-    ques: "Who is a mentor?",
-    ans: "A mentor is a person who can support, advise and guide you. They typically take the time to get to know you and the challenges you’re facing and then use their understanding and personal experience to help you improve.",
+    ques: "How much time do I need to commit as a mentor?",
+    ans: "The time commitment varies depending on your availability and the mentee's needs. Typically, mentors spend a few hours per month in one-on-one sessions or group interactions."
   },
-  // {
-  //   ques: "Who is a mentor?",
-  //   ans: "A mentor is a person who can support, advise and guide you. They typically take the time to get to know you and the challenges you’re facing and then use their understanding and personal experience to help you improve.",
-  // },
-  // {
-  //   ques: "Who is a mentor?",
-  //   ans: "A mentor is a person who can support, advise and guide you. They typically take the time to get to know you and the challenges you’re facing and then use their understanding and personal experience to help you improve.",
-  // },
+  {
+    ques: "Is prior mentoring experience required?",
+    ans: "While prior mentoring experience can be beneficial, it is not a strict requirement. A genuine willingness to help others and share your knowledge and experiences are more important qualities."
+  },
+  {
+    ques: "Can I be a mentor in multiple areas or subjects?",
+    ans: "Yes, if you have expertise and experience in multiple areas, you can choose to mentor in more than one field. However, it's essential to ensure that you can dedicate enough time and attention to each mentee.",
+  },
+  {
+    ques: "Can I be a mentor in multiple areas or subjects?",
+    ans: "Yes, if you have expertise and experience in multiple areas, you can choose to mentor in more than one field. However, it's essential to ensure that you can dedicate enough time and attention to each mentee.",
+  },
+  {
+    ques: "Will mentors receive payment after each session or on a monthly basis?",
+    ans: " Mentors get the option to choose between receiving payment after each session or on a monthly basis.",
+  },
 ];

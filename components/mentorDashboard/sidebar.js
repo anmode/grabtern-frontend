e033e8c1bd8c4512b23eaa20897f6426af80c8ce
Rishu <rrishu561@gmail.com>
2023-07-30 18:49:11 +0530

    feat: navbar for mobile devices

--- components/mentorDashboard/sidebar.js
+++ components/mentorDashboard/sidebar.js
@@ -14,12 +14,14 @@ import { CgProfile } from "react-icons/cg";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { CgMailOpen } from "react-icons/cg";
 import { BiGift } from "react-icons/bi";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import Logo from "../../public/assets/img/favicon1.ico";
 import Image from "next/image";
 import styled from "styled-components";
 
 const Sidebar = ({ setComponent, component }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
     console.log("toggle clicked ", isSidebarOpen);
@@ -97,6 +99,30 @@ const Sidebar = ({ setComponent, component }) => {
     },
   ];
 
+  const mobileItem = [
+    {
+      title: "Profile",
+      icon: <CgProfile />,
+      path: "profile",
+    },
+    {
+      title: "Home",
+      icon: <AiOutlineHome />,
+      path: "sessions",
+    },
+    {
+      title: "Bookings",
+      icon: <LuPhoneCall />,
+      path: "bookings",
+    },
+    {
+      title: "Priority DM",
+      icon: <BiMessageRoundedDots />,
+      path: "queries",
+    },
+  ];
+
+
   const BookButton = styled.button`
     margin: 5px;
     :hover {
@@ -148,18 +174,45 @@ const Sidebar = ({ setComponent, component }) => {
             </ul>
           </div>
         </aside>
+      </div>
 
+      <div className="min-[513px]:tw-hidden">
         {/* For mobile devices max-w-512px */}
-
-        {/* <aside>
-          <div className="tw-fixed min-[513px]:tw-hidden tw-flex tw-justify-center tw-items-center tw-bottom-0 tw-left-0 tw-right-0 tw-text-black tw-z-40 tw-bg-gray-200">
+        <aside className="tw-fixed tw-flex tw-justify-around tw-flex-wrap tw-items-center tw-bottom-0 tw-left-0 tw-right-0 tw-text-black tw-z-40 tw-bg-gray-200">
+          <div className="tw-flex tw-justify-center tw-items-center tw-mr-10 max-[400px]:tw-hidden">
             <div className="tw-flex tw-justify-between tw-items-center tw-p-4">
-            <RxRocket className="group-hover:tw-text-primary-100 tw-text-xl" />
+              <RxRocket className="group-hover:tw-text-primary-100 tw-text-xl" />
             </div>
+            <span className="tw-text-gray-600 tw-text-lg">|</span>
           </div>
-        </aside> */}
+          <div className={`tw-gap-8 tw-flex tw-font-medium tw-py-2`}>
+            {mobileItem.map((val, key) => (
+              <HoverListItem
+                key={key}
+                className="tw-flex tw-group tw-cursor-pointer hoverList"
+              >
+                <div
+                  className={`tw-flex tw-flex-wrap ${component === val.path ? "tw-bg-[#00C9A7]" : ""} tw-p-2 hover:tw-bg-[#00C9A7] group-hover:tw-text-primary-100 tw-gap-5 tw-transition-all tw-text-xl tw-duration-150 tw-ease-in-out tw-items-center tw-text-gray-900 tw-rounded-lg`}
+                  onClick={() => setComponent(val.path)}
+                >
+                  <span>{val.icon}</span>
+                  <span className={`tw-ml-3 ${isSidebarOpen ? "tw-block" : "tw-hidden"}`}>{val.title}</span>
+                </div>
+              </HoverListItem>
+            ))}
+          </div>
+          <div className="tw-flex tw-justify-center tw-items-center">
+            {/* a button for expanding the remaining buttons */}
+            <button className="tw-flex tw-justify-center tw-items-center">
+              {
+                isMobileSidebarOpen ? <AiOutlineClose className="tw-text-2xl tw-text-gray-900" onClick={() => setIsMobileSidebarOpen(false)} /> : <AiOutlineMenu className="tw-text-2xl tw-text-gray-900" onClick={() => setIsMobileSidebarOpen(true)} />
+              }
+            </button>
+          </div>
+        </aside>
 
       </div>
+
     </>
   );
 };

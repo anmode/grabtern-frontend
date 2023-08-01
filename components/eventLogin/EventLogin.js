22fbf235c1c652f55154e0bd50a96018abf93b0a
Illaf <ilafshafeeq@gmail.com>
2023-08-01 21:07:21 +0530

    feat: fixed spinner in mobile device

--- components/eventLogin/EventLogin.js
+++ components/eventLogin/EventLogin.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import styles from "../../styles/form.module.css"
 function EventLogin() {
   return (
     <div>
-       <img
+       <img   className={styles.spinner}
                   style={{ width: "50px", height: "50px",color:"black" }}
                   src="/assets/img/gif/Spinner.gif"
                   alt="...jljk"

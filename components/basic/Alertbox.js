f672b9a8712416137a73783b7cf23bb715b63211
Illaf <ilafshafeeq@gmail.com>
2023-08-03 23:50:32 +0530

    fix: added changes

--- components/basic/Alertbox.js
+++ components/basic/Alertbox.js
@@ -1,33 +1,45 @@
 import { React, useState, useEffect } from "react";
 
 const AlertBox = ({ message, redirectTo }) => {
-  const [time, setTime] = useState(5);
-
+  const [progress, setProgress] = useState(0);
+  const redirectTime = 3;
   useEffect(() => {
-    const countdown = setInterval(() => {
-      setTime((prevTime) => prevTime - 1);
-    }, 1000);
+    const progressInterval = 1000 / 10;
+    const steps = redirectTime * 10;
+    let currentStep = 0;
 
-    return () => clearInterval(countdown);
-  }, []);
+    const timer = setInterval(() => {
+      currentStep += 1;
+      const currentProgress = (currentStep / steps) * 100;
+      setProgress(currentProgress);
 
-  useEffect(() => {
-    if (time === 0) {
-      window.location.href = redirectTo;
-    }
-  }, [time]);
+      if (currentStep === steps) {
+        clearInterval(timer);
+        window.location.href = redirectTo;
+      }
+    }, progressInterval);
+    return () => clearInterval(timer);
+  }, [redirectTime]);
 
   return (
-    <div className="tw-flex tw-justify-center tw-items-center">
-      <div className="alert-box tw-flex  tw-border-2 tw-border-[#6E4FA0] tw-mt-32 tw-h-[32vh] tw-w-[350px] tw-rounded-lg tw-bg-[#FFFFFF]">
-        <div className="tw-flex tw-flex-col tw-justify-center tw-m-5">
-          <div className="alert-content tw-relative tw-top-4 tw-mb-7 tw-flex tw-justify-center tw-font-semibold tw-text-[24px] ">
-            <p className="tw-text-[#6E4FA0]">{message}</p>
+    <>
+      <div className="tw-flex tw-justify-center tw-items-center">
+        <div className="alert-box tw-flex  tw-border-2 tw-border-[#6E4FA0] tw-mt-32 tw-h-[107px] tw-w-[350px] tw-justify-center tw-items-center tw-rounded-lg tw-bg-[#FFFFFF]">
+          <div className="tw-flex tw-flex-col tw-justify-center tw-m-2 tw-w-[90%]">
+            <div className="alert-content tw-relative tw-top-2 tw-w-full tw-mb-7 tw-flex tw-justify-center tw-text-[19px] ">
+              <p className="tw-text-[#6E4FA0]">{message}</p>
+            </div>
+            {/* <p>Redirecting you in {time} seconds</p> */}
+            <div className="progress-bar tw-relative tw-w-full tw-h-2 tw-bg-gray-200 tw-rounded">
+              <div
+                className="progress-bar-fill tw-absolute tw-top-0 tw-left-0 tw-h-full tw-bg-[#8461bd] tw-rounded-md"
+                style={{ width: `${progress}%` }}
+              />
+            </div>
           </div>
-          <p>Redirecting you in {time} seconds</p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

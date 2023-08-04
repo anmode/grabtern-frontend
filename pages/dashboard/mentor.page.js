25146f71f842dfd6b6c0b30b2c485420aab9cd7a
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-08-04 13:04:36 +0530

    fixes

--- pages/dashboard/mentor.page.js
+++ pages/dashboard/mentor.page.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import styles from "../../styles/sidebar.module.css";
 import Sidebar from "../../components/mentorDashboard/sidebar";
 import Profile from "../../components/mentorDashboard/profile";
 import Sessions from "../../components/mentorDashboard/sessions";
@@ -12,26 +10,35 @@ import Bookings from "../../components/mentorDashboard/Bookings";
 
 function MentorDashboard() {
   // getting page name on change in tab
-  const [component, setComponent] = useState("sessions");
-  // useEffect(() => {
-  //   const search = window.location.search;
-  //   const params = new URLSearchParams(search);
-  //   setComponent(params.get("tab") || "");
-  // }, [window.location.search]);
-  const updatePath = (componentName) => {
-    const newPath = `/dashboard/mentor/${componentName}`;
-    window.history.pushState({}, "", newPath);
-    setComponent(componentName);
-  };
+  const [component, setComponent] = useState("");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [mentor, setMentor] = useState({});
+
+  useEffect(() => {
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    setComponent(params.get("tab") || "");
+  }, [window.location.search]);
   return (
     <>
       <div className="tw-flex">
         {/* <Header navbarBackground={true} /> */}
-        <Sidebar setComponent={updatePath} component={component} />
-        <div>
-          {component == "profile" && <Profile />}
-          {component == "calendar" && <Calendar />}
-          {component == "sessions" && <Sessions />}
+        <Sidebar
+          mentor={mentor}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <div className="">
+          {component === "" && (
+            <Home
+              setIsSidebarOpen={setIsSidebarOpen}
+              mentor={mentor}
+              setMentor={setMentor}
+            />
+          )}
+          {component === "profile" && <Profile />}
+          {component === "calendar" && <Calendar />}
+          {component === "sessions" && <Sessions />}
           {component == "queries" && <Queries />}
           {component == "bookings" && <Bookings />}
         </div>

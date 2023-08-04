0d2bead3512ce9d5b10b01d359fd0048d0d6cdf3
Rishu Raj <rrishu561@gmail.com>
2023-08-04 09:06:03 +0530

    feat: mobile optimization for ``dashboard/mentor`` homepage (#680)
    * feat: init notification for mobile
    
    * feat: mobile opt. dashboard/mentor homepage
    
    * feat: mobile opt. dashboard/mentor homepage
    
    * feat: changes in notification centre
    
    * fix: padding bottom
    
    * fix: padding notification bell

--- components/mentorDashboard/home.js
+++ components/mentorDashboard/home.js
@@ -4,10 +4,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { BiSolidUser, BiTime, BiCalendar } from "react-icons/bi";
 import { BsTwitter, BsLinkedin } from "react-icons/bs";
-import { MdNotifications, MdPayment } from "react-icons/md";
+import { MdNotifications, MdPayment, MdOutlineNotificationsNone } from "react-icons/md";
 
 const Home = ({ setComponent, setIsSidebarOpen, mentor, setMentor }) => {
   const [Notification, setNotification] = useState(false);
+  const [mobileNotification, setMobileNotification] = useState(false);
 
   // 'https://grabtern-backend-demo.vercel.app/api/mentors/mentorDetail/${mentorUsername}';   // GET request to get mentor details
 
@@ -39,6 +40,7 @@ const Home = ({ setComponent, setIsSidebarOpen, mentor, setMentor }) => {
         !reference.current.contains(e.target)
       ) {
         setNotification(false);
+        setMobileNotification(false);
       }
     };
 
@@ -98,7 +100,7 @@ const Home = ({ setComponent, setIsSidebarOpen, mentor, setMentor }) => {
 
   return (
     <>
-      <header className="max-[762px]:tw-justify-center max-[762px]:tw-items-center tw-gap-4 tw-py-10 min-[513px]:tw-pl-28 min-[513px]:tw-pr-12 tw-flex tw-justify-between tw-flex-wrap">
+      <header className="max-[762px]:tw-justify-center max-[762px]:tw-items-center tw-gap-4 tw-py-10 min-[513px]:tw-pl-28 min-[513px]:tw-pr-12 tw-flex tw-justify-between tw-flex-wrap max-[512px]:tw-flex-col">
         <h1 className="tw-text-4xl tw-font-bold">
           Welcome <span>{mentor.name?.split(" ")[0]}</span>!
         </h1>{" "}
@@ -124,16 +126,32 @@ const Home = ({ setComponent, setIsSidebarOpen, mentor, setMentor }) => {
 
           {/* Notification */}
           <div
-            onClick={() => setNotification(!Notification)}
             title="Notifications"
             className="tw-cursor-pointer tw-transition-all tw-duration-200 tw-ease-in-out hover:tw-bg-primary-200 tw-bg-primary-100 tw-justify-center tw-items-center tw-relative tw-p-2 tw-rounded-md"
           >
-            <MdNotifications className="tw-w-7 tw-h-7 tw-text-[#FBEAFF]" />
+            {
+              (Notification || mobileNotification) ? (
+                <MdOutlineNotificationsNone
+                  onClick={() => {
+                    setNotification(false);
+                    setMobileNotification(false);
+                  }}
+                  className="tw-w-[30px] tw-h-[30px] tw-text-[#FBEAFF]" />
+              ) : (
+                <MdNotifications
+                  onClick={() => {
+                    setNotification(true);
+                    setMobileNotification(true);
+                  }}
+                  className="tw-w-[30px] tw-h-[30px] tw-text-[#FBEAFF]" />
+              )
+            }
           </div>
           {Notification && (
+            // Notification Pop-up for devices with width > 512px
             <div
               ref={reference}
-              className="tw-z-50 tw-absolute tw-top-[90px] tw-right-[47px] tw-w-[300px] tw-h-[500px] tw-bg-[#FBEAFF] tw-rounded-md tw-shadow-xl tw-p-4 tw-flex tw-flex-col tw-gap-2 tw-overflow-y-scroll"
+              className="tw-z-50 tw-absolute tw-top-[90px] tw-right-[47px] tw-w-[300px] tw-h-[500px] tw-bg-[#FBEAFF] tw-rounded-md tw-shadow-xl tw-p-4 tw-flex tw-flex-col tw-gap-2 tw-overflow-y-scroll max-[512px]:tw-hidden max-[696px]:tw-top-[150px] max-[696px]:tw-right-[200px]"
             >
               <h2 className="tw-text-2xl tw-font-semibold tw-text-center tw-mb-5">
                 Notifications
@@ -146,16 +164,32 @@ const Home = ({ setComponent, setIsSidebarOpen, mentor, setMentor }) => {
         </div>
       </header>
 
-      <main className="max-[512px]:tw-pl-6 tw-pl-28 tw-flex tw-flex-col max-[708px]:tw-justify-center max-[708px]:tw-items-center">
+      {/* Nnotification for mobile devices < 512px */}
+      {
+        mobileNotification && (
+          <div
+            ref={reference}
+            className="tw-z-50 tw-w-full tw-h-[500px] tw-bottom-0 tw-fixed tw-bg-[#FBEAFF] tw-rounded-md tw-shadow-xl tw-p-4 tw-flex tw-flex-col tw-gap-2 tw-overflow-y-scroll min-[513px]:tw-hidden">
+            <h2 className="tw-text-2xl tw-font-semibold tw-text-center tw-mb-5">
+              Notifications
+            </h2>
+            <p className="tw-text-lg tw-font-medium tw-text-center">
+              Coming Soon...
+            </p>
+          </div>
+        )
+      }
+
+      <main className="max-[512px]:tw-pl-6 tw-pb-14 tw-pl-28 tw-flex tw-flex-col max-[708px]:tw-justify-center max-[708px]:tw-items-center">
         <p className="tw-flex tw-justify-start tw-items-center tw-text-center tw-text-xl tw-font-semibold">
           Here you can view your sessions, edit your profile, and view your
           calendar.
         </p>
-        <div className="tw-flex-wrap tw-mt-10 tw-flex tw-gap-10 max-[512px]:tw-justify-center max-[512px]:tw-items-center">
+        <div className="tw-flex-wrap tw-mt-10 tw-flex tw-gap-10 max-[762px]:tw-justify-center max-[762px]:tw-items-center max-[600px]:tw-flex-col">
           {cards.map((card) => {
             return (
-              <div className="tw-w-[300px] tw-h-[300px] tw-bg-white tw-shadow-xl tw-gap-2 tw-p-6 tw-flex tw-flex-col tw-justify-around tw-items-center tw-rounded-md hover:tw-scale-110 tw-ease-in-out tw-duration-150 tw-transition-all max-[763px]:tw-w-[200px] max-[601px]:tw-w-[150px]">
-                <div className="tw-justify-center tw-items-center tw-flex tw-flex-col tw-gap-2">
+              <div className="tw-w-[300px] tw-flex-wrap tw-bg-white tw-shadow-xl tw-gap-2 tw-p-6 tw-flex tw-justify-around tw-items-center tw-rounded-md hover:tw-scale-110 tw-ease-in-out tw-duration-150 tw-transition-all max-[752px]:tw-w-[500px] max-[686px]:tw-w-[400px] max-[512px]:tw-w-[300px]">
+                <div className="tw-justify-center tw-items-center tw-flex tw-flex-col tw-gap-2 tw-w-full">
                   {card.icon}
                   <h2 className="tw-font-semibold tw-text-xl">{card.name}</h2>
                   {card.socials && (

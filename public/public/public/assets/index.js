b03b5fb5672a768477efde7799a90ac93387a64f
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-08-05 17:09:51 +0530

    feat: making grabtern a progressive web app

--- public/public/public/assets/index.js
+++
@@ -1,6 +0,0 @@
-export { default as MentorImg } from './img/MentorImg.png';
-export { default as BecomeMentor } from './img/BecomeMentor.png';
-export { default as GSoC } from './img/company/GSoC.svg';
-export { default as microsoft } from './img/company/microsoft.svg';
-export { default as mlh } from './img/company/mlh.svg';
-export { default as hackerRank } from './img/company/hackerRank.svg';

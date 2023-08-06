a51c3d6ad91476d2db051b533de9242d5eee22f4
Sayantan Choudhury <127319225+Sch476@users.noreply.github.com>
2023-08-07 00:44:13 +0530

    Update community.page.js

--- pages/community.page.js
+++ pages/community.page.js
@@ -1,3 +1,4 @@
+// community.page.js
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import dynamic from "next/dynamic";
@@ -21,7 +22,6 @@ function Community() {
       });
   }, []);
 
-  // Function to fetch commit count for each contributor
   const fetchCommitCount = (login) => {
     return axios
       .get(`https://api.github.com/repos/anmode/grabtern-frontend/commits`, {

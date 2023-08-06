cb920dfb8a119b77ad70c61c3cb7ab8cd6a36793
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-08-06 23:19:04 +0530

    fix: formatting the code

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const webpack = require("webpack");
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
   // disable: process.env.NODE_ENV === 'development',
 });
-
 
 module.exports = withPWA({
   reactStrictMode: true,
--- pages/_offline.js
+++ pages/_offline.js
@@ -1,12 +1,15 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 const Fallback = () => (
-    <>
-        <Head>
-            <title>Offline</title>
-        </Head>
-        <h1 className='tw-text-center tw-text-2xl tw-items-center tw-font-semibold'>This is offline fallback page. Please turn on your internet to check the website.</h1>
-    </>
-)
+  <>
+    <Head>
+      <title>Offline</title>
+    </Head>
+    <h1 className="tw-text-center tw-text-2xl tw-items-center tw-font-semibold">
+      This is offline fallback page. Please turn on your internet to check the
+      website.
+    </h1>
+  </>
+);
 
-export default Fallback
+export default Fallback;

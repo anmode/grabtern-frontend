5f5409db86cdb3bc259fac972fb7fbc13374d12d
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-08-10 22:04:23 +0530

    prettier fix team JS

--- pages/Teams.js
+++ pages/Teams.js
@@ -1,7 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
-import html2Canvas from 'html2canvas';
-import jsPDF from 'jspdf';
-import { Document, Page, Text, View, StyleSheet, Canvas } from '@react-pdf/renderer';
+import React, { useState, useEffect, useRef } from "react";
+import html2Canvas from "html2canvas";
+import jsPDF from "jspdf";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  Canvas,
+} from "@react-pdf/renderer";
 
 const Teams = () => {
   const pdfRef = useRef();
@@ -12,23 +19,27 @@ const Teams = () => {
   const [startIndex, setStartIndex] = useState(0);
   const commitsPerPage = 10;
 
-  const repoOwner = 'anmode';
-  const repoName = 'grabtern-frontend';
-  const contributorUsername = 'anmode';
+  const repoOwner = "anmode";
+  const repoName = "grabtern-frontend";
+  const contributorUsername = "anmode";
 
   useEffect(() => {
     fetch(`https://api.github.com/users/${contributorUsername}`)
-      .then(response => response.json())
-      .then(data => setContributor(data))
-      .catch(error => console.error('Error fetching contributor:', error));
+      .then((response) => response.json())
+      .then((data) => setContributor(data))
+      .catch((error) => console.error("Error fetching contributor:", error));
 
-    fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/commits?author=${contributorUsername}&per_page=100`)
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      `https://api.github.com/repos/${repoOwner}/${repoName}/commits?author=${contributorUsername}&per_page=100`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
         setCommits(data);
-        setDisplayedCommits(data.slice(startIndex, startIndex + commitsPerPage));
+        setDisplayedCommits(
+          data.slice(startIndex, startIndex + commitsPerPage),
+        );
       })
-      .catch(error => console.error('Error fetching commits:', error));
+      .catch((error) => console.error("Error fetching commits:", error));
   }, []);
 
   const handleNextPage = () => {
@@ -47,8 +58,8 @@ const Teams = () => {
     const input = pdfRef.current;
     html2Canvas(input).then((canvas) => {
       const contributorImage = contributor.avatar_url; // Get contributor's image URL
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4', true);
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4", true);
       const pdfWidth = pdf.internal.pageSize.getWidth();
       const pdfHeight = pdf.internal.pageSize.getHeight();
       const imageWidth = canvas.width;
@@ -58,7 +69,7 @@ const Teams = () => {
       const imgY = 30;
 
       // Add contributor's image to the PDF with adjusted dimensions
-      pdf.addImage(contributorImage, 'JPEG', imgX + 90, imgY - 25, 40, 40);
+      pdf.addImage(contributorImage, "JPEG", imgX + 90, imgY - 25, 40, 40);
 
       // Calculate the appropriate dimensions for the canvas image in the PDF
       const adjustedImageWidth = imageWidth * ratio;
@@ -68,14 +79,17 @@ const Teams = () => {
       const adjustedImgX = (pdfWidth - adjustedImageWidth) / 2;
       const adjustedImgY = imgY + 20; // Adjust Y position to avoid overlap with contributor's image
 
-      pdf.addImage(imgData, 'PNG', adjustedImgX, adjustedImgY, adjustedImageWidth, adjustedImageHeight);
-      pdf.save('Thanks.pdf');
+      pdf.addImage(
+        imgData,
+        "PNG",
+        adjustedImgX,
+        adjustedImgY,
+        adjustedImageWidth,
+        adjustedImageHeight,
+      );
+      pdf.save("Thanks.pdf");
     });
   };
-  
-  
-  
-  
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-400">
@@ -91,9 +105,12 @@ const Teams = () => {
         <div className="bg-gray-500 text-white mt-4 p-4 rounded-lg">
           <h3 className="text-lg font-semibold mb-2">Commits</h3>
           <ul className="space-y-2">
-            {displayedCommits.map(commit => (
+            {displayedCommits.map((commit) => (
               <li key={commit.sha}>
-                <span className="text-gray-300 font-semibold">#{commit.sha.slice(0, 6)}</span> - {commit.commit.message}
+                <span className="text-gray-300 font-semibold">
+                  #{commit.sha.slice(0, 6)}
+                </span>{" "}
+                - {commit.commit.message}
               </li>
             ))}
           </ul>

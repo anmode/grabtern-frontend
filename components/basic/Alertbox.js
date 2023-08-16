be695161f6d7e6ac02f0fa8f7858f48f93fdfe13
Illaf <ilafshafeeq@gmail.com>
2023-08-16 16:37:46 +0530

    fix: prettier run

--- components/basic/Alertbox.js
+++ components/basic/Alertbox.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from "react";
-import {AiFillCheckCircle} from "react-icons/ai"
-const AlertBox = ({ message, redirectTo,color }) => {
+import { AiFillCheckCircle } from "react-icons/ai";
+const AlertBox = ({ message, redirectTo, color }) => {
   const [progress, setProgress] = useState(100);
 
   useEffect(() => {
@@ -23,16 +23,16 @@ const AlertBox = ({ message, redirectTo,color }) => {
     <>
       <div className="tw-flex tw-justify-center tw-items-center">
         <div className="progress-bar tw-flex tw-flex-row tw-relative tw-w-[330px] tw-h-[40px] tw-bg-white ">
-          <div className="tw-text-[#b497e2] tw-w-10 tw-h-5 tw-absolute tw-left-0"><AiFillCheckCircle className="tw-w-10 tw-h-10"/></div>
+          <div className="tw-text-[#b497e2] tw-w-10 tw-h-5 tw-absolute tw-left-0">
+            <AiFillCheckCircle className="tw-w-10 tw-h-10" />
+          </div>
           <div className="alert-content tw-absolute tw-text-black  tw-mb-7 tw-flex  tw-text-[19px] ">
             <p className="tw-text-black ">{message}</p>
-            </div>
+          </div>
           <div
             className="progress-bar-fill tw-relative tw-top-[37px]  tw-h-[10%] tw-bg-[#b497e2] "
-            style={{ width: `${progress}%`, backgroundColor:color }}
-          >
-            
-          </div>
+            style={{ width: `${progress}%`, backgroundColor: color }}
+          ></div>
         </div>
       </div>
     </>

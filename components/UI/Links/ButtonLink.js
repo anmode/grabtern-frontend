d5e421d91b2ec2af8bdfe87ac2561c3064da1930
sayyedarib <sayyedaribhussain4321@gmail.com>
2023-09-07 22:41:54 +0530

    add spinner in button while loading
    Signed-off-by: sayyedarib <sayyedaribhussain4321@gmail.com>

--- components/UI/Links/ButtonLink.js
+++ components/UI/Links/ButtonLink.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 import clsx from "clsx";
 import styles from "./Link.module.css";
@@ -10,6 +10,7 @@ function ButtonLink({
   variant = "Primary",
   size = "normal",
 }) {
+  const [isLoading, setIsLoading] = useState(false);
   let variantClass = `link${variant.charAt(0).toUpperCase()}${variant.slice(
     1,
   )}`;
@@ -18,10 +19,22 @@ function ButtonLink({
       href={href}
       className={clsx(
         `${styles.linkBtn} ${styles[variantClass]} ${className}`,
-        size == "sm" && ["tw-text-xs tw-font-medium"],
+        size == "sm" && ["tw-text-xs tw-font-medium tw-items-center tw-flex tw-content-center"],
       )}
+      onClick={()=>{setIsLoading(true);setTimeout(()=>setIsLoading(false), 60000)}}
     >
-      {text}
+      {isLoading? 
+      <div className="tw-flex tw-justify-center tw-items-center">
+
+      <img
+                    style={{ maxWidth:"100%", height:"20px"}}
+                    src="/assets/img/gif/Spinner.gif"
+                    alt="...loader"
+                    />
+                    </div>
+                    :text      
+
+                }
     </Link>
   );
 }

fc00dca427177db1435446e826cd71a628adaf0e
muhammadjufry <sayyidmuhammad878@gmail.com>
2023-09-25 23:48:48 -0700

    fix: dashboard issues

--- components/mentorDashboard/Bookings.js
+++ components/mentorDashboard/Bookings.js
@@ -80,60 +80,24 @@ const Bookings = ({ setLoadingState, setErrorState }) => {
       </nav>
 
       {/* sessions list */}
-      <div className="tw-mt-8">
-        <ul className="tw-bg-primary-100 tw-text-white  tw-flex tw-items-center tw-gap-6 tw-p-6 tw-border tw-border-b-0 tw-rounded-t">
-          <li className="tw-hidden min-[540px]:tw-inline">Topic</li>
-          <li className="min-[540px]:tw-hidden">Session List</li>
-          <li className="tw-hidden sm:tw-inline">Mentee</li>
-          <div className="tw-hidden md:tw-grid md:tw-justify-between md:tw-grid-cols-[5rem_5rem]">
-            <li>Day</li>
-            <li className="tw-text-right">Time</li>
-          </div>
-          <li className="tw-hidden min-[540px]:tw-inline tw-text-right md:tw-hidden">
-            Mentoring On
-          </li>
-        </ul>
-        <ul className="tw-bg-white tw-border tw-rounded-b">
-          {sessions ? (
-            sessions
-              .filter((session) =>
-                activeTab.toLocaleLowerCase() === "pending"
-                  ? session.isbooked
-                  : !session.isbooked,
-              )
-              .map((session, index) => (
-                <li
-                  key={index}
-                  className="min-[540px]:tw-grid min-[540px]:tw-grid-cols-[auto_7rem] min-[540px]:tw-justify-between sm:tw-justify-normal tw-p-4 sm:tw-grid-cols-[auto_8rem] md:tw-grid-cols-[auto_13rem] tw-text-base-400 tw-border-b last:tw-border-b-0 tw-gap-6"
-                >
-                  <div className="sm:tw-grid min-[540px]:tw-grid-cols-[auto_8rem] sm:tw-grid-cols-[auto_8rem] tw-gap-6">
-                    <p className="tw-font-medium">{session.sessionName}</p>
-                    <p className="tw-capitalize sm:tw-hidden">
-                      Mentee • {session.userName}
-                    </p>
-                    <p className="tw-capitalize tw-hidden sm:tw-block">
-                      {session.userName}
-                    </p>
-                  </div>
-                  <div>
-                    <div className="md:tw-grid tw-hidden md:tw-grid-cols-[5rem_5rem] md:tw-justify-between">
-                      <p className="tw-capitalize">{session.sessionDay}</p>
-                      <p className="tw-uppercase tw-text-right">
-                        {session.sessionTime}
-                      </p>
-                    </div>
-                    <p className="tw-capitalize min-[540px]:tw-text-right md:tw-hidden">
-                      {session.sessionDay.substring(0, 3)},&nbsp;
-                      {session.sessionTime}
-                    </p>
-                  </div>
-                </li>
-              ))
-          ) : (
-            <Spinner />
-          )}
-        </ul>
-      </div>
+      <table className="tw-mt-8">
+        <thead className="tw-bg-primary-100 tw-text-white tw-border tw-rounded-t tw-w-full">
+          <tr className="tw-w-full">
+            <th className="tw-p-2">Topic</th>
+            <th className="tw-p-2">Mentor</th>
+            <th className="tw-p-2">Day</th>
+            <th className="tw-p-2">Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="tw-bg-white tw-border tw-w-full tw-rounded-b">
+            <td className="tw-w-full p-2">1-1 mentorship</td>
+            <td className="tw-w-full p-2">Muhammad</td>
+            <td className="tw-w-full p-2">Tuesday</td>
+            <td className="tw-w-full p-2">03:00</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };

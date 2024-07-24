d9cb5d3c7e171536a2be273219702af7edb716cd
code-par-learn <uthra0864@gmail.com>
2024-07-24 10:05:18 +0530

    style: ran Prettier to fix formatting issues

--- components/support/SupportChat.js
+++ components/support/SupportChat.js
@@ -17,7 +17,7 @@ const SupportChat = () => {
       setIsOpen(false);
     }
   };
- 
+
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
     return () => {
@@ -272,190 +272,194 @@ const SupportChat = () => {
 
             {/* for Mobile devices */}
             <div className="sm:tw-hidden tw-mb-24 mob_mode tw-mx-4 tw-flex tw-flex-col tw-items-end ">
-            <div className="sm:tw-hidden tw-mb-1.5 tw-rounded-md tw-flex tw-flex-col tw-bg-cyan-200 tw-overflow-y-scroll tw-overflow-x-hidden tw-justify-between tw-cursor-auto">
-              {/* header */}
-              <div className="tw-py-3.5 tw-flex tw-text-center tw-justify-between tw-w-full tw-items-center tw-top-0 tw-sticky tw-bg-cyan-200 tw-z-40">
-                <div className="tw-flex tw-items-center tw-justify-center">
-                  <Image src={logo} alt="logo" width={55} height={55} />
-                  <h1 className="tw-text-md tw-font-bold tw-text-cyan-700">
-                    Support
-                  </h1>
+              <div className="sm:tw-hidden tw-mb-1.5 tw-rounded-md tw-flex tw-flex-col tw-bg-cyan-200 tw-overflow-y-scroll tw-overflow-x-hidden tw-justify-between tw-cursor-auto">
+                {/* header */}
+                <div className="tw-py-3.5 tw-flex tw-text-center tw-justify-between tw-w-full tw-items-center tw-top-0 tw-sticky tw-bg-cyan-200 tw-z-40">
+                  <div className="tw-flex tw-items-center tw-justify-center">
+                    <Image src={logo} alt="logo" width={55} height={55} />
+                    <h1 className="tw-text-md tw-font-bold tw-text-cyan-700">
+                      Support
+                    </h1>
+                  </div>
+                  <p className="tw-text-center tw-items-center tw-flex tw-justify-center tw-mr-9">
+                    {userData?.user_name || mentorData?.mentor_name}
+                  </p>
                 </div>
-                <p className="tw-text-center tw-items-center tw-flex tw-justify-center tw-mr-9">
-                  {userData?.user_name || mentorData?.mentor_name}
-                </p>
-                
-              </div>
 
-              {/* body */}
-              <div
-                className={`tw-flex tw-flex-col ${
-                  isMentorLoggedIn || isUserLoggedIn ? "" : "tw-justify-center"
-                }`}
-              >
-                {isUserLoggedIn || isMentorLoggedIn ? (
-                  <div className="tw-flex tw-flex-col tw-mr-3 tw-h-[300px] tw-overflow-y-scroll tw-overflow-x-hidden tw-gap-1 tw-text-center tw-w-full">
-                    {messages.length === 0 && (
-                      <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-gap-3">
-                        <Image
-                          src={chatSupport}
-                          alt="chatSupport"
-                          width={120}
-                          height={120}
-                        />
-                        <h1 className="tw-text-indigo-400 tw-font-bold tw-text-xl">
-                          No messages yet.
-                        </h1>
-                        <p>Start the conversation.</p>
-                      </div>
-                    )}
-                    {/* messages */}
-                    {messages.map((message, index) => (
-                      <div
-                        key={index}
-                        className={`tw-relative tw-flex tw-flex-col tw-mr-3 tw-ml-2 ${
-                          message.sender === "support"
-                            ? "tw-text-left tw-justify-start"
-                            : "tw-text-right tw-justify-end tw-items-end"
-                        } tw-gap-1`}
-                      >
-                        {message?.image && (
+                {/* body */}
+                <div
+                  className={`tw-flex tw-flex-col ${
+                    isMentorLoggedIn || isUserLoggedIn
+                      ? ""
+                      : "tw-justify-center"
+                  }`}
+                >
+                  {isUserLoggedIn || isMentorLoggedIn ? (
+                    <div className="tw-flex tw-flex-col tw-mr-3 tw-h-[300px] tw-overflow-y-scroll tw-overflow-x-hidden tw-gap-1 tw-text-center tw-w-full">
+                      {messages.length === 0 && (
+                        <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-gap-3">
                           <Image
-                            onClick={() =>
-                              window.open(URL.createObjectURL(message.image))
-                            }
-                            src={URL.createObjectURL(message?.image)}
-                            alt="Uploaded Image"
+                            src={chatSupport}
+                            alt="chatSupport"
                             width={120}
                             height={120}
-                            className="tw-rounded-sm"
                           />
-                        )}
+                          <h1 className="tw-text-indigo-400 tw-font-bold tw-text-xl">
+                            No messages yet.
+                          </h1>
+                          <p>Start the conversation.</p>
+                        </div>
+                      )}
+                      {/* messages */}
+                      {messages.map((message, index) => (
+                        <div
+                          key={index}
+                          className={`tw-relative tw-flex tw-flex-col tw-mr-3 tw-ml-2 ${
+                            message.sender === "support"
+                              ? "tw-text-left tw-justify-start"
+                              : "tw-text-right tw-justify-end tw-items-end"
+                          } tw-gap-1`}
+                        >
+                          {message?.image && (
+                            <Image
+                              onClick={() =>
+                                window.open(URL.createObjectURL(message.image))
+                              }
+                              src={URL.createObjectURL(message?.image)}
+                              alt="Uploaded Image"
+                              width={120}
+                              height={120}
+                              className="tw-rounded-sm"
+                            />
+                          )}
 
-                        {message?.text && (
-                          <div
-                            className={`${
-                              message.text.length > 20
-                                ? "tw-w-[200px] tw-bg-indigo-400"
-                                : "tw-w-[100px] tw-bg-indigo-400"
-                            } tw-rounded-md tw-p-2 tw-m-1 tw-text-white tw-text-sm tw-break-words tw-shadow-lg tw-opacity-80 hover:tw-opacity-100 ${
-                              isOpen ? "tw-opacity-100" : "tw-opacity-80"
-                            }`}
-                          >
-                            {message?.text}
-                          </div>
-                        )}
-                        {message?.time && (message?.text || message?.image) && (
-                          <p className="tw-text-xs tw-ml-2 tw-mb-0.5 tw-text-gray-500">
-                            {message.time.toLocaleTimeString()}
-                          </p>
+                          {message?.text && (
+                            <div
+                              className={`${
+                                message.text.length > 20
+                                  ? "tw-w-[200px] tw-bg-indigo-400"
+                                  : "tw-w-[100px] tw-bg-indigo-400"
+                              } tw-rounded-md tw-p-2 tw-m-1 tw-text-white tw-text-sm tw-break-words tw-shadow-lg tw-opacity-80 hover:tw-opacity-100 ${
+                                isOpen ? "tw-opacity-100" : "tw-opacity-80"
+                              }`}
+                            >
+                              {message?.text}
+                            </div>
+                          )}
+                          {message?.time &&
+                            (message?.text || message?.image) && (
+                              <p className="tw-text-xs tw-ml-2 tw-mb-0.5 tw-text-gray-500">
+                                {message.time.toLocaleTimeString()}
+                              </p>
+                            )}
+                        </div>
+                      ))}
+                    </div>
+                  ) : (
+                    <div className="tw-flex tw-flex-col tw-justify-around tw-items-center tw-gap-3">
+                      <Image
+                        src={noSupport}
+                        alt="noSupport"
+                        width={120}
+                        height={120}
+                      />
+                      <h1 className="tw-text-indigo-400 tw-font-bold tw-text-xl">
+                        You are not logged in.
+                      </h1>
+                      <Link
+                        href={"/contact"}
+                        className="tw-font-semibold tw-no-underline hover:tw-text-indigo-400"
+                      >
+                        Contact Us
+                      </Link>
+                      <p>OR</p>
+                      <h2>
+                        Please{" "}
+                        <Link
+                          className="tw-text-indigo-400 hover:tw-text-indigo-500 tw-ease-in-out"
+                          href="/auth/login?entityType=user"
+                        >
+                          Sign In
+                        </Link>{" "}
+                        to chat with us.
+                      </h2>
+                    </div>
+                  )}
+                </div>
+
+                {/* form */}
+                <form
+                  onSubmit={handleTextSubmit}
+                  className="tw-flex tw-flex-col tw-m-2 tw-sticky tw-bottom-0  tw-gap-2 tw-text-center tw-justify-between tw-w-full tw-items-start tw-top-0 tw-bg-cyan-200  tw-z-40"
+                >
+                  {imagePreview && (
+                    <div
+                      className={`tw-rounded-sm tw-bg-slate-300 tw-p-1 tw-flex tw-gap-3`}
+                    >
+                      <Image
+                        src={imagePreview}
+                        alt="Image Preview"
+                        width={120}
+                        height={120}
+                      />
+                      <AiOutlineCloseCircle
+                        onClick={() => {
+                          setImagePreview(null);
+                          setImageValue(null);
+                        }}
+                        className="tw-w-6 tw-h-6 tw-relative tw-text-red-500 hover:tw-text-red-600 tw-ease-in-out tw-transition-all"
+                        title="cancel"
+                      />
+                    </div>
+                  )}
+                  {isMentorLoggedIn ||
+                    (isUserLoggedIn && (
+                      <div className="tw-flex tw-text-center tw-justify-between tw-w-full tw-items-center tw-py-2 tw-z-40 tw-gap-2">
+                        <label htmlFor="imageValue" className="tw-relative">
+                          <BiSolidImageAdd className="tw-w-6 tw-h-6 tw-text-indigo-400 hover:tw-text-indigo-500 tw-ease-in-out tw-transition-all tw-cursor-pointer tw-items-center tw-text-center tw-justify-center tw-flex" />
+                        </label>
+                        <input
+                          className="tw-hidden"
+                          id="imageValue"
+                          type="file"
+                          onChange={handleImageChange}
+                        />
+                        <input
+                          className="tw-p-1 tw-w-full tw-rounded-md tw-border tw-border-gray-300 tw-placeholder-gray-500 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-blue-400 focus:tw-border-transparent"
+                          type="text"
+                          value={inputValue}
+                          onChange={(event) =>
+                            setInputValue(event.target.value)
+                          }
+                        />
+
+                        {/* Only when mentor or user is logged in, can send the message */}
+                        {isMentorLoggedIn || isUserLoggedIn ? (
+                          <button title="send" type="submit">
+                            <MdSend className="tw-w-6 tw-h-6 tw-text-indigo-400 tw-mr-3" />
+                          </button>
+                        ) : (
+                          <button type="button">
+                            <Link
+                              className="tw-bg-indigo-400 tw-w-1/2 tw-text-sm tw-rounded-sm tw-font-semibold tw-text-white tw-p-1 hover:tw-bg-indigo-500 tw-ease-in-out"
+                              href="/"
+                            >
+                              Sign In
+                            </Link>
+                          </button>
                         )}
                       </div>
                     ))}
-                  </div>
-                ) : (
-                  <div className="tw-flex tw-flex-col tw-justify-around tw-items-center tw-gap-3">
-                    <Image
-                      src={noSupport}
-                      alt="noSupport"
-                      width={120}
-                      height={120}
-                    />
-                    <h1 className="tw-text-indigo-400 tw-font-bold tw-text-xl">
-                      You are not logged in.
-                    </h1>
-                    <Link
-                      href={"/contact"}
-                      className="tw-font-semibold tw-no-underline hover:tw-text-indigo-400"
-                    >
-                      Contact Us
-                    </Link>
-                    <p>OR</p>
-                    <h2>
-                      Please{" "}
-                      <Link
-                        className="tw-text-indigo-400 hover:tw-text-indigo-500 tw-ease-in-out"
-                        href="/auth/login?entityType=user"
-                      >
-                        Sign In
-                      </Link>{" "}
-                      to chat with us.
-                    </h2>
-                  </div>
-                )}
+                </form>
               </div>
 
-              {/* form */}
-              <form
-                onSubmit={handleTextSubmit}
-                className="tw-flex tw-flex-col tw-m-2 tw-sticky tw-bottom-0  tw-gap-2 tw-text-center tw-justify-between tw-w-full tw-items-start tw-top-0 tw-bg-cyan-200  tw-z-40"
+              <button
+                className="tw-flex tw-bg-primary-100 tw-items-center tw-justify-center tw-w-10 tw-h-10  tw-text-white tw-rounded-full tw-focus:outline-none"
+                onClick={() => setIsOpen(false)}
               >
-                {imagePreview && (
-                  <div
-                    className={`tw-rounded-sm tw-bg-slate-300 tw-p-1 tw-flex tw-gap-3`}
-                  >
-                    <Image
-                      src={imagePreview}
-                      alt="Image Preview"
-                      width={120}
-                      height={120}
-                    />
-                    <AiOutlineCloseCircle
-                      onClick={() => {
-                        setImagePreview(null);
-                        setImageValue(null);
-                      }}
-                      className="tw-w-6 tw-h-6 tw-relative tw-text-red-500 hover:tw-text-red-600 tw-ease-in-out tw-transition-all"
-                      title="cancel"
-                    />
-                  </div>
-                )}
-                {isMentorLoggedIn ||
-                  (isUserLoggedIn && (
-                    <div className="tw-flex tw-text-center tw-justify-between tw-w-full tw-items-center tw-py-2 tw-z-40 tw-gap-2">
-                      <label htmlFor="imageValue" className="tw-relative">
-                        <BiSolidImageAdd className="tw-w-6 tw-h-6 tw-text-indigo-400 hover:tw-text-indigo-500 tw-ease-in-out tw-transition-all tw-cursor-pointer tw-items-center tw-text-center tw-justify-center tw-flex" />
-                      </label>
-                      <input
-                        className="tw-hidden"
-                        id="imageValue"
-                        type="file"
-                        onChange={handleImageChange}
-                      />
-                      <input
-                        className="tw-p-1 tw-w-full tw-rounded-md tw-border tw-border-gray-300 tw-placeholder-gray-500 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-blue-400 focus:tw-border-transparent"
-                        type="text"
-                        value={inputValue}
-                        onChange={(event) => setInputValue(event.target.value)}
-                      />
-
-                      {/* Only when mentor or user is logged in, can send the message */}
-                      {isMentorLoggedIn || isUserLoggedIn ? (
-                        <button title="send" type="submit">
-                          <MdSend className="tw-w-6 tw-h-6 tw-text-indigo-400 tw-mr-3" />
-                        </button>
-                      ) : (
-                        <button type="button">
-                          <Link
-                            className="tw-bg-indigo-400 tw-w-1/2 tw-text-sm tw-rounded-sm tw-font-semibold tw-text-white tw-p-1 hover:tw-bg-indigo-500 tw-ease-in-out"
-                            href="/"
-                          >
-                            Sign In
-                          </Link>
-                        </button>
-                      )}
-                    </div>
-                  ))}
-              </form>
-              
-            </div>
-
-            <button  className="tw-flex tw-bg-primary-100 tw-items-center tw-justify-center tw-w-10 tw-h-10  tw-text-white tw-rounded-full tw-focus:outline-none"  onClick={() => setIsOpen(false)}>
-             <span className="tw-text-xl">✖</span>
+                <span className="tw-text-xl">✖</span>
               </button>
             </div>
-
-            
           </>
         ) : (
           <>
